66002e61b8071afd389792a17b2abba2c98dee15
Stefano Alverino <stefanoalverino@gmail.com>
2023-10-07 16:58:22 +0200

    Abbozzata leggiFile e struct

--- L01/E02/main.c
+++ L01/E02/main.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 #define MAXL 30
-
+#define MAXR 1000
 //Variabili globali
 typedef enum{r_date, r_partenza, r_capolinea, r_ritardo,
              r_ritardo_tot, r_fine, r_errore}comando_e;
+typedef struct{
+    char codice_tratta[MAXL];
+    char partenza[MAXL];
+    char destinazione[MAXL];
+    int data[3];
+    int ora_partenza[3];
+    int ora_arrivo[3];
+    int ritardo;
+}sTratta;
 
 //Funzioni
 comando_e leggiComando(void);
+int leggiFile(char *nomeFile, sTratta tratte[MAXR]);
 int main(void) {
     //Inizializzazione variabili
     comando_e comando;
+    int nr=0;
+    sTratta tratte[MAXL];
+    char nomeFile[MAXR];
     //Apertura file
+    printf("Inserisci il nome del file:");
+    scanf(" %s", nomeFile);
+    //nr = leggiFile(nomeFile, tratte);
     //Lettura Comando
     comando = leggiComando();
+    printf("Il comando vale: %d", comando);
     //Corpo programma
     //Chiusura file
     return 0;
@@ -26,7 +43,7 @@ comando_e leggiComando(void){
 
     //Corpo funzione
     printf("Inserisci comando:");
-    scanf(" %s ", comando);
+    scanf(" %s", comando);
     //r_date, r_partenza, r_capolinea, r_ritardo,
     //             r_ritardo_tot, r_fine, r_errore
     if(strcmp("date", comando) == 0){
@@ -53,3 +70,4 @@ comando_e leggiComando(void){
     }
     return comandoE;
 }
+
