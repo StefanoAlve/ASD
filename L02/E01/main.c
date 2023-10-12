692a2267dcbd0ba13fc16df99e1f73c1ff5e83c7
Stefano Alverino <stefanoalverino@gmail.com>
2023-10-12 11:59:30 +0200

    Ambiente L02

---
+++ L02/E01/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Esercizio 1 L02\n");
+    return 0;
+}
---
+++ L02/E02/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Esercizio 2 L02\n");
+    return 0;
+}
