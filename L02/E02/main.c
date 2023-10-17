8715a2dfd1a6bfebf990e42d07c7a8640c7630d9
Gianbattistabsn <gianbattista.busonera@studenti.polito.it>
2023-10-17 10:51:46 +0200

    L2E2 pulito

--- L02/E02/main.c
+++ L02/E02/main.c
@@ -1,44 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define file_name "mat.txt"
 
-int **malloc2dR(int nr, int nc);
-void leggi_file(char* name_file, int ***mat, int* nr, int* nc);
+int **malloc2dR(int nr, int nc); // alloca lo spazio per righe e colonne di una matrice (che viene ritornata)
+void free2d(int **mat, int nr); // libera lo spazio precedentemente allocato alla matrice
+int **leggi_file(char* name_file, int* nr, int* nc); // legge il file e ritorna la matrice con i valori presenti nel file
 void stampa_mat(int **mat, int nr, int nc);
-void separa(int ** mat, int nr, int nc, int **vb, int ** vn, int numB, int numN);
-void conta_caselle(int nr, int nc, int *nb, int *nn);
+void separa(int ** mat, int nr, int nc, int **vettB, int **vettN, int numB, int numN);
+void conta_caselle(int nr, int nc, int *nb, int *nn); // conta il numero di caselle bianche e nere
 void stampa_vet(int *vett, int n);
 
 
 int main(void)
 {
     int **mat, *vettB, *vettN, nr, nc, numB, numN;
-    char name_file[8] = {'m','a','t','.','t','x','t', '\0'};
-    leggi_file(name_file, &mat, &nr, &nc);
+    mat = leggi_file(file_name, &nr, &nc);
+    printf("La matrice presente nel file e' la seguente:\n");
     stampa_mat(mat, nr, nc);
     conta_caselle(nr, nc, &numB, &numN);
     separa(mat, nr, nc, &vettB, &vettN, numB, numN);
-    printf("\nIl vettore contenente gli elementi delle caselle bianche contiene: ");
+    free2d(mat, nr);
+    printf("\nIl vettore contenente gli elementi delle %d caselle bianche contiene: ", numB);
     stampa_vet(vettB, numB);
     free(vettB);
-    printf("\nIl vettore contenente gli elementi delle caselle nere contiene: ");
+    printf("\nIl vettore contenente gli elementi delle %d caselle nere contiene: ", numN);
     stampa_vet(vettN, numN);
     free(vettN);
 }
 
 
-void leggi_file(char* name_file, int ***mat, int* nr, int* nc)
+int **leggi_file(char* name_file, int* nr, int* nc)
 {
     int i, j;
-    int **m;
+    int **m = NULL;
     FILE* fp_in = fopen(name_file, "r");
     if (fp_in != NULL)
     {
         fscanf(fp_in, "%d %d", nr, nc);
         if (nr != NULL && nc != NULL) {
-            *mat = malloc2dR(*nr, *nc);
+            m = malloc2dR(*nr, *nc);
         }
-        m = *mat; // solo per rendere più leggibile nella scanf altrimenti dovrei usare &((*mat)[i][j])
-        if(mat!= NULL)
+        if(m!= NULL)
         {
             for (i = 0; i < *nr; i++)
             {
@@ -47,6 +49,7 @@ void leggi_file(char* name_file, int ***mat, int* nr, int* nc)
             }
         }
     }
+    return m;
 }
 
 int **malloc2dR(int nr, int nc)
@@ -59,6 +62,14 @@ int **malloc2dR(int nr, int nc)
         m[i] = (int *)malloc(nc*sizeof(int));
     return m;
 }
+
+void free2d(int **m, int nr)
+{
+    for(int i = 0; i < nr; i++)
+        free(m[i]);
+    free(m);
+}
+
 void stampa_mat(int **mat, int nr, int nc)
 {
     int i,j;
@@ -72,41 +83,40 @@ void stampa_mat(int **mat, int nr, int nc)
 
 void conta_caselle(int nr, int nc, int *nb, int *nn)
 {
-    int tot = 0;//                                                      b n b
-    tot = nr*nc;//                                                      n b n
-    if (tot % 2 != 0) // se dispari avrei una configurazione del tipo:  b n b
-    {
-        *nb = (int)tot/2 + 1;
-        *nn = tot- *nb;
+    int tot;//                                                           b n b
+    tot = nr*nc;//                                                       n b n
+    if (tot % 2 != 0){ // se dispari avrei una configurazione del tipo:  b n b
+        *nb = (int)tot/2 + 1; *nn = tot- *nb;
     }
-    else
-    {
-        *nb = tot/2;
-        *nn = tot/2;
+    else{
+        *nb = tot/2; *nn = tot/2;
     }
 }
 
 void separa(int ** mat, int nr, int nc, int **vettB, int **vettN, int numB, int numN)
 {
     int i,j, contB = 0, contN = 0;
-    *vettB = (int *)malloc(numB*sizeof(int));
-    *vettN = (int *)malloc(numN * sizeof(int));
+    int *vB, *vN;
+    vB = (int *)malloc(numB*sizeof(int));
+    vN = (int *)malloc(numN * sizeof(int));
     for (i = 0; i < nr; i++)
     {
         for (j = 0; j < nc; j++)
         {
             if ((i+j)%2 == 0)
             {
-                (*vettB)[contB] = mat[i][j];
+                vB[contB] = mat[i][j];
                 contB++;
             }
             else
             {
-                (*vettN)[contN] = mat[i][j];
+                vN[contN] = mat[i][j];
                 contN++;
             }
         }
     }
+    *vettB = vB;
+    *vettN = vN;
 }
 
 void stampa_vet(int *vett, int n)
