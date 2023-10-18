79a9eacf918cc14377c2588173d07e3975f401ac
Alessio <s314162@studenti.polito.it>
2023-10-18 12:56:47 +0200

    Modifica1 E03: definizione variabili globali

--- L01/E03/main.c
+++ L01/E03/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAXN 30
+
+typedef struct{
+    char codiceTratta[MAXN];
+    char partenza[MAXN];
+    char destinazione[MAXN];
+    char data[MAXN];
+    char o_partenza[MAXN];
+    char o_arrivo[MAXN];
+    int ritardo;
+} struct_tratte;
+
+typedef enum {
+    stampa_log, ord_data_ora, ord_codTratta, ord_partenza, ord_destinazione, search_tratta_partenza, invalid
+} comando_e;
 
 int main() {
-    printf("Hello, World!\n");
+
     return 0;
 }
