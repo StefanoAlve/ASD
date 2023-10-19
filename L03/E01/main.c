b730e6ba165fadbf6acc1fe3752433b8664c969b
Stefano Alverino <stefanoalverino@gmail.com>
2023-10-19 11:35:48 +0200

    E01 L03 fatto, possibili miglioramenti

--- L03/E01/main.c
+++ L03/E01/main.c
@@ -1,6 +1,78 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Esercizio 1 L03\n");
+int majority(int *a, int N);
+
+int main(void) {
+    //Inizializzazione variabili
+    int mag, *a, N;
+    //Corpo programma
+    printf("Inserisci numero di elementi:");
+    scanf("%d", &N);
+    //Allocazione dinamica del vettore
+    a = (int*)malloc(N*sizeof(int));
+
+    for(int i = 0; i<N; i++){
+        printf("Inserisci valore della cella %d:", i+1);
+        scanf("%d", &a[i]);
+    }
+    mag = majority(a, N);
+    if(mag >= 0){
+        printf("Il maggioritario vale: %d\n", mag);
+    }
+    else{
+        printf("Non esiste un maggioritario\n");
+    }
+    //Deallocazione vettore
+    free(a);
     return 0;
+}
+
+int majority(int *a, int N){
+    int m = N/2, lMajority, rMajority, leftCount = 0, rightCount = 0;
+
+    if(N==0){
+        //Il caso in cui la dimensione del vettore fosse 0 non esiste maggioritario
+        return -1;
+    }
+
+    if(N==1){
+        //Caso limite, sottovettori di dimensione 1 il maggioritario è per forza il numero
+        return a[0];
+    }
+
+    //Spezzo il vettore in sottovettori
+    lMajority = majority(a, m);
+    rMajority = majority(a+m, N-m);
+
+    if(lMajority == rMajority){
+        //se il maggioritario di entrambi i sottovettori è lo stesso allora sarà il maggioritario del vettore intero
+        return lMajority;
+    }
+    else{
+        //Nel caso in cui fossero diversi vado a cercare nel vettore quale dei due compare più volte
+        for(int i = 0; i<N; i++){
+            if(a[i] == lMajority){
+                leftCount++;
+            }
+            else if(a[i] == rMajority){
+                rightCount++;
+            }
+        }
+
+        if(leftCount > m){
+            //Se lMajority compare più volte del valore della metà del vettore allora lo restituisco
+            return lMajority;
+        }
+
+        if(rightCount > m){
+            //Se rMajority compare più volte del valore della metà del vettore allora lo restituisco
+            return rMajority;
+        }
+
+        //Se non trovo un maggioritario ritorno -1
+        return -1;
+    }
+
+
 }
