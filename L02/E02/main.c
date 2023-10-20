0ace133a8d8b0142cff30b958b79e190d3da2d12
giaca <s312871@studenti.polito.it>
2023-10-20 13:04:26 +0200

    Codice Terminato

--- L02/E02/main.c
+++ L02/E02/main.c
@@ -4,6 +4,8 @@
 
 int **malloc2dR(int nr, int nc, FILE *inFile);
 void deAlloca(int **matrice, int nr);
+void separa(int **matrix, int nr, int nc, int **Vbianche, int *dimBianche, int **Vnere, int *dimNere);
+void stampaArray(int *array, int dimArray);
 
 int main() {
     int nr, nc, **matrix;
@@ -21,7 +23,23 @@ int main() {
     //Allocazione e riempimento matrice
     matrix = malloc2dR(nr, nc, inFile);
 
+    //Caselle bianche e nere
+    int *VBianche, *VNere, dimNere, dimBianche;
+
+    separa(matrix, nr, nc, &VBianche, &dimBianche, &VNere, &dimNere);
+
+    printf("VETTORE CELLE NERE: ");
+    stampaArray(VNere, dimNere);
+
+    printf("VETTORE CELLE BIANCHE: ");
+    stampaArray(VBianche, dimBianche);
+
+
+
+    //Deallocazione e chiusura file
     fclose(inFile);
+    free(VNere);
+    free(VBianche);
     deAlloca(matrix, nr);
 
     return 0;
@@ -62,5 +80,58 @@ void deAlloca(int **matrice, int nr){
     for(int i = 0; i < nr; i++) free(matrice[i]);
 
     free(matrice);
+}
+
+//Separa celle bianche e nere della matrice in due vettori
+void separa(int **matrix, int nr, int nc, int **Vbianche, int *dimBianche, int **Vnere, int *dimNere){
+    // Contatori per tenere traccia delle dimensioni dei vettori
+    int countB = 0;
+    int countN = 0;
+
+    // Scansione della matrice e conteggio delle caselle bianche e nere
+    for(int i = 0; i < nr; i++){
+        for(int j = 0; j < nc; j++){
+
+            // Determina se la cella è bianca o nera in base a riga/colonna pari/dispari
+            if((i + j) % 2 == 0) countB++;
+
+            else countN++;
+        }
+    }
+
+    // Alloca dinamicamente i vettori per le caselle bianche e nere
+    *Vbianche = (int *)malloc(countB * sizeof(int));
+    *Vnere = (int *)malloc(countN * sizeof(int));
+
+    // Indici dei vettori
+    int indB = 0;
+    int indN = 0;
+
+    // Riempimento dei vettori con i dati delle caselle bianche e nere
+    for(int i = 0; i < nr; i++){
+        for(int j = 0; j < nc; j++){
+            if((i + j) % 2 == 0){
+                (*Vbianche)[indB] = matrix[i][j];
+                indB++;
+            }
+            else{
+                (*Vnere)[indN] = matrix[i][j];
+                indN++;
+            }
+        }
+    }
+
+    // Imposta le dimensioni dei vettori di caselle bianche e nere
+    *dimBianche = countB;
+    *dimNere = countN;
+}
+
+//Stampa vettore
+void stampaArray(int *array, int dimArray){
+
+    for(int i = 0; i < dimArray; i++){
+        printf("%d ", array[i]);
+    }
+    printf("\n");
 }
 
