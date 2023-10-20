63963a174bc0c5b01950eec8d9eb14b7ba3adbee
giaca <s312871@studenti.polito.it>
2023-10-20 10:30:31 +0200

    codice finito

--- L01/E01/main.c
+++ L01/E01/main.c
@@ -3,17 +3,19 @@
 #define MAX 51
 
 int match(char *src, char *regexp);
-char *cercaRegexp(char *src, char *regexp);
+char* cercaRegexp(char *src, char *regexp);
+void parentesiQuadre(char **src, char **regexp, int *found);
+void maiuscMinusc(char **src, char**regexp, int *found);
 
-int main() {
-    char source[MAX], regexp[MAX];
+int main(){
+    char src[MAX], regexp[MAX];
 
     //input
     printf("Inserisci stringa da trovare e espressione regolare:\n");
-    scanf("%s %s", source, regexp);
+    scanf("%s %s", src, regexp);
 
     //corrispondenze
-    char *corrispondenza = cercaRegexp(source, regexp);
+    char *corrispondenza = cercaRegexp(src, regexp);
 
     //output
     if (corrispondenza != NULL)
@@ -25,93 +27,107 @@ int main() {
 }
 
 //funzione principale per cercare un'espressione regolare in source
-char *cercaRegexp(char *src, char *regexp){
-    while (*src){
-        if (match(src, regexp))
+char* cercaRegexp(char *src, char *regexp){
+
+    //itero interamente su src, carattere per carattere fino a NULL
+    while(*src){
+        if(match(src, regexp))
             return src; //restituisce il puntatore alla corrispondenza trovata
+
+        //vado avanti in src
         src++;
     }
     return NULL; //nessuna corrispondenza trovata
 }
 
 //verifica se source corrisponde all'espressione regolare
-int match(char *src, char *regexp){
+int match(char *src, char *regexp) {
     int found = 1;
+
     //itero su tutta regexp
-    while (*regexp && found){
+    while(*regexp && found){
 
         //il punto corrisponde a qualsiasi carattere, quindi continua la ricerca
-        if (*regexp == '.'){
-            src++; regexp++;
-        }
+        if(*regexp == '.') src++, regexp++;
 
-        else if (*regexp == '['){
+            //Caso parentesi
+        else if(*regexp == '['){
             //avanza di un carattere dopo '['
             regexp++;
-
-            if(*regexp == '^'){
-                //il carattere successivo non deve essere presente
-                regexp++;
-
-                while(*regexp != ']'){
-
-                    if (*src == *regexp)
-                        found = 0; //il carattere non va bene
-
-                    regexp++;
-                }
-            }
-            else{
-                found = 0;
-                //il carattere successivo deve essere presente
-                while(*regexp != ']'){
-
-                    if(*src == *regexp)
-                        found = 1; //il carattere va bene
-
-                    regexp++;
-                }
-            }
-            //avanti dopo ']'
-            regexp++;
-            src++;
+            //gestione parentesi
+            parentesiQuadre(&src, &regexp, &found);
         }
-
-            // controllo maiuscole/minuscole
+            //Caso \a oppure \A
         else if(*regexp == '\\'){
+            //avanza dopo '\'
             regexp++;
+            //gestione maiuscole/minuscole
+            maiuscMinusc(&src, &regexp, &found);
 
-            //controllo maiuscole nell'espressione regolare
-            if(isupper(*regexp)){
-
-                //se nella frase non abbiamo una maiuscola found = 0
-                if( !isupper(*src) )
-                    found = 0;
-            }
-
-                //controllo minuscole
-            else if(islower(*regexp)) {
-
-                //se nella frase non abbiamo una minuscola found = 0
-                if (!islower(*src))
-                    found = 0;
-            }
-            else
-                found = 0; //errore
-
-            //dopo il controllo vado avanti
-            src++;
-            regexp++;
         }
 
-        else if(*src != *regexp){
+            //Controllo se si hanno gli stessi caratteri
+        else if (*src != *regexp) {
             found = 0; //carattere diverso, nessuna corrispondenza
         }
-        else{
+
+            //Se si ha corrispondenza si va avanti
+        else {
             //continua la ricerca
-            src++;
-            regexp++;
+            src++, regexp++;
         }
     }
     return found;
+}
+
+//Controllo carattere in presenza di parentesi quadre
+void parentesiQuadre(char **src, char **regexp, int *found){
+
+    if(**regexp == '^'){
+        //il carattere successivo non deve essere presente
+        (*regexp)++;
+
+        while(**regexp != ']'){
+
+            if (*src == *regexp)
+                *found = 0; //il carattere non va bene
+
+            (*regexp)++;
+        }
+    }
+    else{
+        *found = 0;
+        //il carattere successivo deve essere presente
+        while(**regexp != ']'){
+            if(**src == **regexp)
+                *found = 1; //il carattere va bene
+
+            (*regexp)++;
+        }
+    }
+    //avanti dopo ']'
+    (*regexp)++, (*src)++;
+}
+
+void maiuscMinusc(char **src, char**regexp, int *found){
+
+    //Controllo maiuscole nell'espressione regolare
+    if(isupper(**regexp)){
+
+        //se nella src non abbiamo una maiuscola found = 0
+        if (!isupper(**src))
+            *found = 0;
+    }
+
+        //Controllo minuscole
+    else if(islower(**regexp)){
+
+        //se nella frase non abbiamo una minuscola found = 0
+        if(!islower(**src))
+            *found = 0;
+    }
+    else *found = 0; //errore
+
+    //dopo il controllo vado avanti
+    (*src)++, (*regexp)++;
 }
