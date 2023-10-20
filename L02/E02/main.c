777d0ae9bb257c4d9eac21f8acdd5e5fc316d6bf
giaca <s312871@studenti.polito.it>
2023-10-20 12:11:23 +0200

    Struttura codice, allocazione e deallocazione matrice

--- L02/E02/main.c
+++ L02/E02/main.c
@@ -1,6 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define nomeFile "mat.txt"
+
+int **malloc2dR(int nr, int nc, FILE *inFile);
+void deAlloca(int **matrice, int nr);
 
 int main() {
-    printf("Esercizio 2 L02\n");
+    int nr, nc, **matrix;
+    FILE *inFile;
+
+    //Apertura file
+    if((inFile = fopen(nomeFile, "r")) == NULL){
+        printf("Errore nell'apertura del file.\n");
+        exit(1);
+    }
+
+    //Dimensione matrice
+    fscanf(inFile, "%d %d", &nr, &nc);
+
+    //Allocazione e riempimento matrice
+    matrix = malloc2dR(nr, nc, inFile);
+
+    fclose(inFile);
+    deAlloca(matrix, nr);
+
     return 0;
 }
+
+//Allocazione e riempimento matrice
+int **malloc2dR(int nr, int nc, FILE *inFIle){
+    int **matrix;
+
+    //Allocazione colonne
+    matrix = (int **)malloc(nr * sizeof(int *));
+    if(matrix == NULL){
+        printf("Errore nell'allocazione delle colonne\n");
+        exit(1);
+    }
+    //Allocazione righe
+    for (int i = 0; i < nr; i++) {
+        matrix[i] = (int *)malloc(nc * sizeof(int));
+        if(matrix[i] == NULL){
+            printf("Errore nell'allocazione delle righe\n");
+            exit(1);
+        }
+    }
+
+    //Riempimento matrice
+    for(int i = 0; i < nr; i++){
+        for(int j = 0; j < nc; j++){
+            fscanf(inFIle, "%d ", &matrix[i][j]);
+        }
+    }
+
+    return matrix;
+}
+
+//Dealloca la matrice
+void deAlloca(int **matrice, int nr){
+
+    for(int i = 0; i < nr; i++) free(matrice[i]);
+
+    free(matrice);
+}
+
