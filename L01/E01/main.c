8b632e3ced89a8b700d5fedbafaf90e53fbcabf6
Stefano Alverino <stefanoalverino@gmail.com>
2023-10-23 08:43:09 +0200

    L01 E01 rifatto

--- L01/E01/main.c
+++ L01/E01/main.c
@@ -4,7 +4,7 @@
 
 #define MAXL 100
 
-char *cercaRegexp(char *src, char *regexp);
+char *cercaRegexp(char *src, char *regexp, int regexp_lenght);
 int cercaLenghtRegexp(char *regexp);
 
 
@@ -18,92 +18,78 @@ int main(void)
     printf("Inserisci l'espressione regolare da cercare:");
     scanf("%s", regexp);
     lenght = cercaLenghtRegexp(regexp);
-    printf("La regular expression è lunga: %d", lenght);
-    regexpPointer = cercaRegexp(source, regexp);
+    printf("La regular expression e' lunga: %d", lenght);
+    regexpPointer = cercaRegexp(source, regexp, lenght);
     printf("\nIl puntatore indica la prima occorrenza regolare:%s", regexpPointer);
     return 0;
 }
 
 
 
-char *cercaRegexp(char *src, char *regexp)
-{
+char *cercaRegexp(char *src, char *regexp, int regexp_lenght){
+    int i,j = 0, pos, equals = 0, flag = 0, uscita = 0;
     char* regexpPointer = NULL;
-    int i, j, regexpLenght, flag, equals = 0, first = 1, pos = 0;
-    // Cerco la lunghezza effettiva della Regular Expression:
-    regexpLenght = cercaLenghtRegexp(regexp);
-    if ((int)strlen(src) - regexpLenght >= 0) // Condizione utile per verificare che sia possibile cercare nella source
-        for(i = 0; i < strlen(src) && equals != regexpLenght; i++) //verifico di non sforare la stringa e che non abbia trovato un match
-        {
-            flag = 0;
-            for (j = pos; j < strlen(regexp) && !flag; j++)
-            {
-                /// CASO PUNTO
-                if (regexp[j] == '.' && (isalpha(src[i]) || isdigit(src[i]))) // Se c'è un punto mi va bene qualsiasi carattere alfanumerico
+    if (strlen(src)-regexp_lenght >= 0){
+        for (i = 0; i <= strlen(src) - regexp_lenght; i++){
+            pos = 0;
+            uscita = 0;
+            while(regexp[j] != '\0' && !uscita){
+                flag = 0;
+                /// CASO UGUAGLIANZA SEMPLICE
+                if(regexp[j] == src[i+pos])
                     flag = 1;
-                    /// CASI /A E /a
-                else if (regexp[j] == '\\')
-                    if ((regexp[j + 1] == 'A' && isupper(src[i])) || (regexp[j + 1] == 'a' && islower(src[i])))
-                    {
+                /// CASO '.'
+                if(regexp[j] == '.' && (isalpha(src[i+pos]) || isalnum(src[i+pos])))
+                    flag = 1;
+                    /// CASO '\A' o '\a'
+                else if(regexp[j] == '\\'){
+                    j++; // considero il carattere A o a
+                    if(regexp[j] == 'A' && isupper(src[i+pos]))
                         flag = 1;
-                        pos++;
-                    }
-                        /// CASI PARENTESI QUADRE
-                    else if (regexp[j] == '[')
-                    {
-                        if(regexp[j+1] == '^')
-                        {
-                            flag = 1;
-                            for(int k = j+2; regexp[k] != ']'; k++) // Inizio a cercare dal carattere successivo all'apice
-                            {
-                                if(regexp[k] == src[i] && flag)
-                                    flag = 0;
-                                pos++;
-                            }
-                            pos+=2; //incrementa la posizione sia per l'^ che per la ]
-                        }
-                        else
-                        {
-                            for(int k = j+1; k < pos; k++) //[abc] aasa basa casa
-                            {
-                                if(regexp[k] == src[i] && !flag)
-                                    flag = 1;
-                                pos++;
-                            }
-                            pos++;
-                        }
-                    }
-                        /// UGUAGLIANZA SEMPLICE
-                    else if (regexp[j] == src[i])
+                    else if(regexp[j] == 'a' && islower(src[i+pos]))
                         flag = 1;
-                /// GESTIONE DELLE CONDIZIONI
-                if(flag) {
-                    equals++;
-                    pos++;
                 }
-                else {
+                    /// CASI []
+                else if(regexp[j] == '['){
+                    j++; // considero il carattere successivo
+                    /// CASO CON APICE [^]
+                    if(regexp[j] == '^'){
+                        flag = 1; j++; // considero il carattere successivo all'apice
+                        for (int k = j; regexp[k] != ']' && flag; k++){
+                            if(regexp[k] == src[i+pos])
+                                flag = 0;
+                            j++; // sia k che j alla fine del ciclo saranno pari a regexp[k] = ']'
+                        }
+                    }
+                        /// CASO []
+                    else{
+                        for (int k = j; regexp[k] != ']'; k++){
+                            if(regexp[k] == src[i+pos])
+                                flag = 1;
+                            j++; // sia k che j alla fine del ciclo saranno pari a regexp[k] = ']'
+                        }
+                    }
+                }
+                /// CONDIZIONI
+                if(flag){
+                    j++;
+                    pos++;
+                    equals++;
+                }
+                else{
                     equals = 0;
-                    pos = 0;
-                    flag = 1; // esce dal for più interno
+                    uscita = 1;
+                    j = 0;
                 }
             }
-            /// CASI DI MATCH
-            if (equals == regexpLenght){
-                if(first) {
-                    printf("\nLe parole che contengono espressioni regolari sono:\n");
-                    regexpPointer = &src[i - regexpLenght + 1];
-                    first = 0;
-                }
-                for (int k = 0; k < regexpLenght; k++)
-                    printf("%c", src[i-regexpLenght+1+k]);
-                printf("; ");
-                equals = 0;
-                pos = 0;
+            if (equals == regexp_lenght){
+                regexpPointer=&src[i];
+                return regexpPointer;
             }
         }
+    }
     else
-        printf("\nImpossibile ricercare l'espressione regolare nella stringa di origine");
-
+        printf("\nErrore, impossibile confrontare la stringa e l'espressione regolare.");
     return regexpPointer;
 }
 
@@ -111,18 +97,15 @@ char *cercaRegexp(char *src, char *regexp)
 // DETERMINO LA LUNGHEZZA DELLA STRINGA "REGOLARE" DA CERCARE
 int cercaLenghtRegexp(char *regexp) {
     int lenght = 0, j;
-    for (j = 0; j < strlen(regexp); j++)
-    {
-        if (regexp[j] == '[')
-        {
+    for (j = 0; j < strlen(regexp); j++){
+        if (regexp[j] == '['){
             while (regexp[j] != ']')
                 j++;
             lenght++;
         }
         else if (regexp[j] == '.' || isalpha(regexp[j]))
             lenght++;
-        else if (regexp[j] == '\\' && (regexp[j+1] == 'a' || regexp[j+1] == 'A'))
-        {
+        else if (regexp[j] == '\\' && (regexp[j+1] == 'a' || regexp[j+1] == 'A')){
             lenght++;
             j = j+1;
         }
