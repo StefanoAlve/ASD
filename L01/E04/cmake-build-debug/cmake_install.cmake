44b1dead93f2c9c4bb5ca4cc7c7219d6f35af5c6
AdrianB19 <s300588@studenti.polito.it>
2023-10-25 12:34:41 +0200

    Esercizio 4 lab 01 finito con file di output e multi-ordinamento

--- L01/E04/cmake-build-debug/cmake_install.cmake
+++ L01/E04/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/ASD/L01/E04
+# Install script for directory: C:/Users/bonio/Desktop/POLITO 2023-2024/ASD/L01/E04
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/stefa/AppData/Local/JetBrains/CLion 2022.3.2/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/bonio/AppData/Local/Programs/CLion/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/ASD/L01/E04/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/bonio/Desktop/POLITO 2023-2024/ASD/L01/E04/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
