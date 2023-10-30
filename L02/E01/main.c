de051dd857a3124f5d57ce82355f41a004ac5365
niccoloforese <s312253@studenti.polito.it>
2023-10-30 16:21:19 +0100

    Esercizio 1 LAB 2

--- L02/E01/main.c
+++ L02/E01/main.c
@@ -1,6 +1,49 @@
 #include <stdio.h>
 
+int gcd1(int x, int y) {
+    if (x > y) {
+        if (x % 2 == 0 && y % 2 == 0) {
+            return 2 * (gcd1((x / 2), (y / 2)));
+        }
+        else if (x % 2 != 0 && y % 2 == 0) {
+            return gcd1(x, (y / 2));
+        }
+        else if (x % 2 != 0 && y % 2 != 0) {
+            return gcd1((x - y) / 2, y);
+        }
+        else if(x%2 ==0 && y%2 !=0) {
+            return gcd1(x / 2, y);
+        }
+    }
+    else if (x<y)
+    {
+        if (x % 2 == 0 && y % 2 == 0)
+        {
+            return 2 * (gcd1((y / 2), (x / 2)));
+        }
+        else if (x % 2 != 0 && y % 2 == 0)
+        {
+            return gcd1(x, y/2);
+        }
+        else if(x%2==0 && y%2!=0){
+                return gcd1(y / 2, x);
+        }
+        else if (x % 2 != 0 && y % 2 != 0)
+        {
+            return gcd1(x , (y - x) / 2);
+        }
+
+    }
+    else if(x == y)
+        return x;
+
+}
+
 int main() {
-    printf("Esercizio 1 L02\n");
-    return 0;
+    int num1, num2, franco;
+    printf("Digita qui in numeri di cui vuoi sapere il massimo comun divisore: ");
+    scanf("%d %d", &num1, &num2);
+    franco = gcd1(num1,num2);
+    printf("il minimo comune multiplo tra %d e %d: \n", num1, num2);
+    printf("%d", franco);
 }
