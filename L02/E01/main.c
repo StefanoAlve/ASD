37f741c0267dfcfbdea6f158b2f939b0d56f8dcc
AdrianB19 <s300588@studenti.polito.it>
2023-11-01 17:10:25 +0100

    Lab 02 es 01

--- L02/E01/main.c
+++ L02/E01/main.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <math.h>
-#include <ctype.h>
 
 int gcd(int a, int b);
 
@@ -24,24 +22,22 @@ int main() {
         a = b;
         b = temp;
     }
-    printf("\nCalcolo del MCD ricorsivamente.\na=%d\nb=%d",a,b);
+    printf("\nCalcolo del MCD ricorsivamente.\na=%d\nb=%d", a, b);
     mcd = gcd(a, b);
     printf("\nIl massimo comune divisore tra %d e %d e': %d", a, b, mcd);
     return 0;
 }
 
 int gcd(int a, int b) {
-    if (a % b == 0) {//cond terminazione
+    if (a % b == 0) {// cond terminazione
         return b;
-    }
-    if(a%2==0 && b%2==0){//a,b pari
-        return 2*gcd(a/2,b/2);
-    }else if(a%2!=0 && b%2==0){//a dispari,b pari
-        return gcd(a,b/2);
-    }
-    else if(a%2!=0 && b%2!=0){//a,b dispari
-        return gcd(((a-b)/2),b);
-    }else{//a pari, b dispari
-        return gcd(a/2,b);
+    } else if (a % 2 == 0 && b % 2 == 0) { //a e b  pari
+        return 2 * gcd(a / 2, b / 2);
+    } else if (a % 2 != 0 && b % 2 == 0) { //a dispari e b pari
+        return gcd(a, b / 2);
+    } else if (a % 2 == 0 && b % 2 != 0) { //a pari e b dispari
+        return gcd(a / 2, b);
+    } else { //a,b disp
+        return gcd((a - b) / 2, a);
     }
 }
