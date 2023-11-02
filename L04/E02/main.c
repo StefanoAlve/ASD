e5552e29bbbaac1892998a2bf8e58354430ed7a8
Stefano Alverino <stefanoalverino@gmail.com>
2023-11-02 22:48:19 +0100

    Aggiunta newNode, acquisisci e deallocaPersona

--- L04/E02/main.c
+++ L04/E02/main.c
@@ -4,27 +4,31 @@
 #define MAXC 51
 //Strutture
 typedef struct{
-    char codice[6];
-    char nome[MAXC];
-    char cognome[MAXC];
-    char data_di_nascita[11];
-    char via[MAXC];
-    char citta[MAXC];
+    char *codice;
+    char *nome;
+    char *cognome;
+    char *data_di_nascita;
+    char *via;
+    char *citta;
     int cap;
 }Item;
-
 typedef struct{
-    Item val;
-    struct node *link;
+    Item persona;
+    struct node *next;
 }node, *link;
 
 typedef enum{r_acquisizione,r_ricerca,r_cancellazione,r_stampa, r_fine, r_errore}comando_e;
 typedef enum{i_file, i_tastiera, i_errore}input_e;
+typedef enum{o_codice, o_date, o_errore}opzione_e;
 //Funzioni
 comando_e leggiComando();
 input_e leggiInput();
 void sceltaComando(comando_e comando);
-int leggiFile(FILE *fp);
+opzione_e leggiOpzione();
+link newNode(Item persona, link next);
+Item acquisisci(FILE *fp);
+void deallocaPersona(Item persona);
+
 //newnode
 int main(void) {
     //Inizializzazione variabili
@@ -68,8 +72,10 @@ comando_e leggiComando(){
 }
 void sceltaComando(comando_e comando){
     input_e input;
-    int nr;
+    opzione_e opzione;
     FILE *fp;
+    Item persona;
+    char tmp[MAXC], *nomeFile;
     switch (comando){
         case r_acquisizione:
             do {
@@ -79,22 +85,43 @@ void sceltaComando(comando_e comando){
 
             switch (input){
                 case i_file:
-                    nr = leggiFile(fp);
-                    for(int i=0; i<nr; i++){
-                        acquisisci(nomeFile);
+                    printf("Inserisci il nome del file:");
+                    scanf("%s", tmp);
+                    nomeFile = strdup(tmp);
+                    fp = fopen(nomeFile, "r");
+                    if(fp == NULL){
+                        printf("Errore nell'apertura del file!\n");
+                        exit(1);
+                    }
+                    while(!feof(fp)){
+                        persona = acquisisci(fp);
+                        deallocaPersona(persona);
                     }
                     fclose(fp);
+                    free(nomeFile);
                     break;
                 case i_tastiera:
-                    acquisisci(stdin);
-                    break;
-                case i_errore:
+                    printf("Inserisci i dati anagrafici <codice> <nome> <cognome> <data_di_nascita> <via> <citta'> <cap>:");
+                    persona = acquisisci(stdin);
+                    deallocaPersona(persona);
                     break;
             }
             break;
         case r_ricerca:
             break;
         case r_cancellazione:
+            do {
+                printf("Vuoi cancellare in base al codice o alle date?:");
+                opzione = leggiOpzione();
+            }while(opzione == o_errore);//Finchè l'utente non inserirà un parametro corretto continuerò a chiedere
+            switch (opzione){
+                case o_codice:
+                    //cancellaCodice();
+                    break;
+                case o_date:
+                    //cancellaDate();
+                    break;
+            }
             break;
         case r_stampa:
             break;
@@ -121,12 +148,71 @@ input_e leggiInput(){
         printf("Tipo di input inesistente, riprova!\n");
         inputE = i_errore;
     }
+    free(input);
     return inputE;
 }
 
-int leggiFile(FILE *fp){
+opzione_e leggiOpzione(){
+    opzione_e opzioneE;
+    char tmp[MAXC], *opzione;
+    scanf(" %s", tmp);
+    opzione = strdup(tmp);
 
-    return nr;
+    if(strcasecmp(opzione, "codice") == 0){
+        opzioneE = o_codice;
+    }
+    else if(strcasecmp(opzione, "date") == 0){
+        opzioneE = o_date;
+    }
+    else{
+        printf("Tipo di opzione inesistente, riprova!\n");
+        opzioneE = o_errore;
+    }
+    free(opzione);
+    return opzioneE;
+}
+
+link newNode(Item persona, link next){
+    link x;
+    x = (node*)malloc(sizeof(node));
+    if(x == NULL){
+        printf("Errore nell'allocazione della memoria!\n");
+        exit(1);
+    }
+    x->persona = persona;
+    x->next = next;
+    return x;
+}
+
+Item acquisisci(FILE *fp){
+    //Inizializzazione variabili
+    Item persona;
+    char tmp[MAXC];
+    //Corpo programma
+    fscanf(fp," %s",tmp);
+    persona.codice = strdup(tmp);
+    fscanf(fp, " %s", tmp);
+    persona.nome = strdup(tmp);
+    fscanf(fp, " %s", tmp);
+    persona.cognome = strdup(tmp);
+    fscanf(fp, " %s", tmp);
+    persona.data_di_nascita = strdup(tmp);
+    fscanf(fp, " %s", tmp);
+    persona.via = strdup(tmp);
+    fscanf(fp, " %s", tmp);
+    persona.citta = strdup(tmp);
+    fscanf(fp, "%d", &persona.cap);
+
+    return persona;
+}
+
+void deallocaPersona(Item persona){
+    free(persona.codice);
+    free(persona.nome);
+    free(persona.cognome);
+    free(persona.data_di_nascita);
+    free(persona.via);
+    free(persona.citta);
 }
     //acquisizione
         //file:
