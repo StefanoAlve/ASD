058f6e21c18107bfb59011110fea83ab4280932b
Alessio <s314162@studenti.polito.it>
2023-11-04 16:45:35 +0100

    Terminato

--- L03/E01/main.c
+++ L03/E01/main.c
@@ -13,6 +13,7 @@ int main() {
         printf("Non esiste maggioritario in un vettore vuoto\n");
         exit(1);
     }
+    //Allocazione dinamica vettore
     vett = (int *)malloc(len_vett* sizeof(int));
 
     for (int i = 0; i<len_vett; i++){
@@ -33,20 +34,23 @@ int main() {
 
 int majority(int *vett, int len_vett){
     int L_maj, R_maj, Lcont = 0, Rcont = 0, m = len_vett/2;
+    //terminazione quando arrivo al vettore unitario
     if (len_vett==1){
         return vett[0];
     }
 
+    //divisione del vettore in 2 sottovettori
     L_maj = majority(vett,m);
     R_maj = majority(vett+m,len_vett-m);
 
     if (L_maj==R_maj){
-        return R_maj;
-    } else {
-        for (int i = 0; i<len_vett; i++){
+        return R_maj; //Se i due maggioritari sono uguali, ne torno uno dei due (torna -1 quando in entrambi non c'è maggioritario)
+    } else { //Entro nell'else quando sicuramente in almeno un sottovettore c'è maggioritario
+        for (int i = 0; i<len_vett; i++){ //Ricerca lineare nel vettore per trovare corrispondenze del maggioritario
             if (vett[i] == L_maj) Lcont++;
             if (vett[i] == R_maj) Rcont++;
         }
+        //Controllo che il presunto maggioritario del vettore superi la soglia di m
         if (Lcont>m) return L_maj;
         if (Rcont>m) return R_maj;
         return -1;
