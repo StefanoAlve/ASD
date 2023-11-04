2ccfde0ec12a7e9571d2cbc049deef753bd5154f
niccoloforese <s312253@studenti.polito.it>
2023-11-04 01:04:34 +0100

    Esercizio 2 LAB 2 finito e commentato

--- L02/E02/main.c
+++ L02/E02/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "malloc.h"
-int **malloc2dR(int nr,int nc){
+int **malloc2dR(int nr,int nc){// funzione di allocazione di una matrice facendo uso del valore di ritorno per restituire il puntatore alla matrice al main
     int **m;
     int i;
     m =malloc (nr* sizeof(int));
@@ -9,19 +9,19 @@ int **malloc2dR(int nr,int nc){
     }
     return m;
 }
-void free2d(int **m,int nr){
+void free2d(int **m,int nr){ // deallocazione della matrice, prima deallocazione delle celle, e poi di tutta la matrice "vuota"
     int i;
     for(i=0;i<nr;i++)
         free(m[i]);
     free(m);
 }
-void separa(int **mat,int nr,int nc,int **vetW,int **vetB) {
+void separa(int **mat,int nr,int nc,int **vetW,int **vetB) { // funzione ceh separa le celle nere da quelle bianche in base alla posizione nella matrice "scacchiera"
     int i, j, countW = 0,countB=0;
     for (i = 0;i<nr;i++){
         for(j=0;j<nc;j++){
-            if ((i+j)%2==0)
+            if ((i+j)%2==0)// se è in posizione pari aumenta il contatore delle celle bianche
                 countW++;
-            else
+            else// se è in posizione dispari aumenta il contatore delle celle nere
                 countB++;
         }
     }
@@ -30,22 +30,23 @@ void separa(int **mat,int nr,int nc,int **vetW,int **vetB) {
     int B=0,W = 0;
     for (i = 0; i < nr; i++) {
         for (j = 0; j < nc; j++) {
-            if ((i + j) % 2 == 0) {
+            if ((i + j) % 2 == 0) {// se è in posizione pari è una cella bianca
                 (*vetW)[W] = mat[i][j];
                 W++;
             }
-            if ((i + j) % 2 != 0) {
+            if ((i + j) % 2 != 0) {// se è in posizioen dispari è una cella nera
                 (*vetB)[B] = mat[i][j];
                 B++;
             }
         }
     }
-
+//stampa del vettore delle celle bianche
     printf("Nel vettore bianco ci sono: \n");
     for(int k = 0;k<countW;k++){
         printf("%d",(*vetW)[k]);
     }
     printf("\n");
+    //stampa del vettore delle celle nere
     printf("Nel vettore nero ci sono: \n");
     for(int d = 0;d<countB;d++){
         printf("%d",(*vetB)[d]);
@@ -60,23 +61,23 @@ int main() {
     int **matr;
     int *vetW,*vetB;
     FILE *fin;
-    fin = fopen("mat.txt","r");
+    fin = fopen("mat.txt","r");// apertura del file
     if (fin==NULL){
-        printf("Errore in apertura!");
+        printf("Errore in apertura!");// controllo
     }
     else{
-        fscanf(fin,"%d %d",&nr,&nc);
+        fscanf(fin,"%d %d",&nr,&nc);// assunzione del numero di righe e colonne della matrice
     }
-    matr = malloc2dR(nr,nc);
+    matr = malloc2dR(nr,nc);//allocazione della matrice
     for(i = 0;i<nr;i++){
         for(j=0;j<nr;j++){
-            fscanf(fin,"%d",&matr[i][j]);
+            fscanf(fin,"%d",&matr[i][j]);// inserimento nella matrice dei valori letti da file
         }
     }
     separa(matr,nr,nc,&vetW,&vetB);
     free2d(matr,nr);
     fclose(fin);
-    free(vetW);
-    free(vetB);
+    free(vetW);//deallocazione del vettore delle celle bianche
+    free(vetB);//deallocazione del vettore delle celle nere
 }
 
