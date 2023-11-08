d9d0a4e54bf30ae4ee78db4ccc5eb4805f60695a
giaca <s312871@studenti.polito.it>
2023-11-08 20:03:44 +0100

    struttura codice

--- L04/E01/main.c
+++ L04/E01/main.c
@@ -1,8 +1,84 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define nomeFile "grafo.txt"
+
+typedef struct{
+    int Vertice1;
+    int Vertice2;
+}arco;
+typedef struct{int NumVertici; int NumArchi;}dimensioni;
+
+arco *allocaArchi(arco *archi, FILE *fin, dimensioni *dimensione);
+arco *assegnaArchiDaFile(arco *archi, FILE *fin, dimensioni dimensione);
+int vertexCover(arco *archi, dimensioni dimensione, int *sol, int index, int count);
 
 int main() {
+    arco *archi = NULL;
+    dimensioni dimensione;
+    FILE *fin;
+
+    //Apertura file e gestione errori
+    if((fin = fopen(nomeFile, "r")) == NULL){
+        printf("ERRORE LETTURA FILE %s", nomeFile);
+        exit(1);
+    }
+
+    //Allocazione dinamica vettore di struct
+    archi = allocaArchi(archi, fin, &dimensione);
+
+    int *sol = calloc(dimensione.NumVertici, sizeof (int));
 
 
+    //Assegnazione valori a struct
+    archi = assegnaArchiDaFile(archi, fin, dimensione);
 
+    //Funzione ricorsiva che cerca i vertex cover
+    int numSol = vertexCover(archi, dimensione, sol, 0, 0);
+
+    if(numSol != 0) printf("Numero di soluzioni: %d", numSol);
+
+//    for(int i = 0; i < dimensione.NumArchi; i++){
+//        printf("%d %d", archi[i].Vertice1, archi[i].Vertice2);
+//        printf("\n");
+//    }
+
+    fclose(fin);
+    free(archi);
     return 0;
+}
+
+//Allocazione dinamica vettore di struct
+arco *allocaArchi(arco *archi, FILE *fin, dimensioni *dimensione){
+
+    fscanf(fin,"%d %d", &(dimensione->NumArchi), &(dimensione->NumVertici));
+
+    //Allocazione vettore di struct gestione di errori
+    archi = (arco*) malloc(dimensione->NumVertici * sizeof (arco));
+    if(archi == NULL){
+        printf("Errore allocazione vettore di struct");
+        exit(2);
+        }
+
+    return archi;
+}
+
+//Assegnazione dei valori dei vertici a struct
+arco *assegnaArchiDaFile(arco *archi, FILE *fin, dimensioni dimensione){
+
+    for(int i = 0; i < dimensione.NumArchi; i++)
+        fscanf(fin, "%d %d", &archi[i].Vertice1, &archi[i].Vertice2);
+
+    return archi;
+}
+
+//Funzione ricorsiva che cerca tutti i vertex cover e chiama una funzione che li stampa
+int vertexCover(arco *archi, dimensioni dimensione,  int *sol, int index, int count){
+
+    if(index >= dimensione.NumVertici){
+        //STAMPA
+    }
+
+    for(int i = 0; i < dimensione.NumVertici; i++){
+
+    }
 }
