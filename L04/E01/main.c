9bd6b8329daa1bb7292d6879294965bf79762758
giaca <s312871@studenti.polito.it>
2023-11-09 10:13:29 +0100

    Codice Terminato (disposizioni ripetute)

--- L04/E01/main.c
+++ L04/E01/main.c
@@ -10,7 +10,8 @@ typedef struct{int NumVertici; int NumArchi;}dimensioni;
 
 arco *allocaArchi(arco *archi, FILE *fin, dimensioni *dimensione);
 arco *assegnaArchiDaFile(arco *archi, FILE *fin, dimensioni dimensione);
-int vertexCover(arco *archi, dimensioni dimensione, int *sol, int index, int count);
+int powerset(arco *archi, dimensioni dimensione, int *sol, int index, int count);
+int controllaVertexCover(arco *archi, dimensioni dimensione,  int *sol);
 
 int main() {
     arco *archi = NULL;
@@ -32,8 +33,8 @@ int main() {
     //Assegnazione valori a struct
     archi = assegnaArchiDaFile(archi, fin, dimensione);
 
-    //Funzione ricorsiva che cerca i vertex cover
-    int numSol = vertexCover(archi, dimensione, sol, 0, 0);
+    //Funzione ricorsiva che cerca i vertex cover attraverso le disposizioni ripetute
+    int numSol = powerset(archi, dimensione, sol, 0, 0);
 
     if(numSol != 0) printf("Numero di soluzioni: %d", numSol);
 
@@ -71,14 +72,41 @@ arco *assegnaArchiDaFile(arco *archi, FILE *fin, dimensioni dimensione){
     return archi;
 }
 
+//Funzione che
+int controllaVertexCover(arco *archi, dimensioni dimensione,  int *sol){
+    int flag = 1;
+
+    for(int i=0; i<dimensione.NumArchi; i++)
+        if ((sol[archi[i].Vertice1]==0) && (sol[archi[i].Vertice2]==0))
+            flag = 0;
+
+    return flag;
+}
+
 //Funzione ricorsiva che cerca tutti i vertex cover e chiama una funzione che li stampa
-int vertexCover(arco *archi, dimensioni dimensione,  int *sol, int index, int count){
+int powerset(arco *archi, dimensioni dimensione,  int *sol, int index, int count){
 
+    //Caso di controllo ed eventuale stampa
     if(index >= dimensione.NumVertici){
-        //STAMPA
+        if(controllaVertexCover(archi, dimensione, sol) == 1){
+            printf("vertex cover:{ ");
+
+            for(int i = 0; i < dimensione.NumVertici; i++)
+                if (sol[i]==1)
+                    printf("%d ", i);
+            printf("}\n");
+
+            return count+1;
+        }
+        return count;
     }
 
-    for(int i = 0; i < dimensione.NumVertici; i++){
+    //Non prende l'elemento index
+    sol[index] = 0;
+    count = powerset(archi, dimensione, sol, index + 1, count);
 
-    }
+    //Backtrack prendendo l'elemento index
+    sol[index] = 1;
+    count = powerset(archi, dimensione, sol, index + 1, count);
+    return count;
 }
