be6675d04a17cec9455987c3698abdac129d9945
Stefano Alverino <stefanoalverino@gmail.com>
2023-11-09 15:53:52 +0100

    E03

--- L04/E03/main.c
+++ L04/E03/main.c
@@ -2,9 +2,25 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-
 #define numPietre 4
 #define MAXC 100
+
+                             ///BREVE COMMENTO DEL RAGIONAMENTO APPLICATO
+/*        Guardando il precedente                 Guardando il successivo      Combinazioni risultanti:
+ * zaffiro -> zaffiro o rubino           |   zaffiro  <- zaffiro o topazio      zz, zr, tz
+ * rubino -> smaraldo o topazio          |   rubino   <- zaffiro o topazio      rs, rt, zr, tr
+ * topazio -> zaffiro o rubino           |   topazio  <- smeraldo o rubino      tz, tr, st, rt
+ * smeraldo -> smaraldo o topazio        |   smeraldo <- smeraldo o rubino      ss, st, rs
+ *
+ * Dalle combinazioni ricavate, si può notare che il numero di zaffiri e smeraldi è un dato indipendente poichè possono essere sempre seguiti
+ * da una pietra del medesimo tipo.
+ * I topazi e i rubini sono in stretta dipendenza tra di loro perchè hanno entrambi una possibile configurazione rt o tr;
+ * notato ciò se il numero di rubini eccede il numero di topazi di più di 1 o viceversa, allora risulta una configurazione impossibile.
+ * Inoltre, notando che se il numero di rubini è inferiore al numero di topazi allora la configurazione deve per forza iniziare con t, si può usare un indice start per evitare
+ * numerose chiamate ricorsive.
+ */
+
+
 typedef struct{
     int z;
     int r;
@@ -15,12 +31,12 @@ typedef struct{
 int leggiFile(pietre **pCollane);
 void stampaCollana(char *sequenza, int n);
 void stampa_collane(pietre *collane, int nCollane);
-void inizializzaRimanenti(int rimanenti[4], pietre collana);
+void inizializzaRimanenti(int rimanenti[numPietre], pietre collana);
 void wrapperTrovaComb(int nCollane, pietre *collane);
-int trovaComb(char *val, char *sol, int rimanenti[4], int n_dist, int dimSol, int pos, int flag, int start);
+int trovaComb(char *val, char *sol, int rimanenti[numPietre], int n_dist, int dimSol, int pos, int flag, int start);
 int controlloValidita(char *sol, int pos);
 int pietreValide(int *restanti);
-void riduciPietreNonValide(int *restanti, int *k);
+void riduciPietre(int *restanti, int *k);
 int controlloStart(int *restanti);
 
 int main(void) {
@@ -110,7 +126,7 @@ int trovaComb(char *val, char *sol, int rimanenti[4], int n_dist, int dimSol, in
                 flag = trovaComb(val, sol, rimanenti, n_dist, dimSol, pos+1, flag, 0);
             }
             rimanenti[i]++;
-            if(flag){
+            if(flag){ //inutile continuare a cercare perchè ho già trovato la soluzione che mi serve
                 return flag;
             }
         }
@@ -152,12 +168,12 @@ int controlloValidita(char *sol, int pos){
 void wrapperTrovaComb(int nCollane, pietre *collane){
     /// Utilizzo un wrapper per contenere la funzione trovaComb in modo da non inserire troppi dati superflui nel main
     int flag, rimanenti[numPietre],dimSol, pos = 0;
-    unsigned long inizio, tempo;
+    float inizio, tempo;
     char val[numPietre] = {'z','r','t','s'}, *sol;
 
     //Corpo funzione
     for(int i = 0; i<nCollane; i++){
-        inizio = clock();
+        inizio = (float)clock();
         printf("\nTest n.%d:\n", i+1);
         inizializzaRimanenti(rimanenti, collane[i]);
         dimSol = rimanenti[0]+rimanenti[1]+rimanenti[2]+rimanenti[3];
@@ -169,12 +185,12 @@ void wrapperTrovaComb(int nCollane, pietre *collane){
                 dimSol--;
             }
             else{
-                riduciPietreNonValide(rimanenti, &dimSol);
+                riduciPietre(rimanenti, &dimSol);
             }
             free(sol);
         }
-        tempo = clock()-inizio;
-        printf("Tempo impiegato: %lu secondi\n",(tempo/1000));
+        tempo = (float)clock()-inizio;
+        printf("Tempo impiegato: %.3f secondi\n",(tempo/1000));
     }
     free(collane);
 }
@@ -189,7 +205,7 @@ int pietreValide(int *restanti){
     return valido;
 }
 
-void riduciPietreNonValide(int *restanti, int *k){
+void riduciPietre(int *restanti, int *k){
     /// Impongo una collana valida imponendo la condizione presentata il pietreValide, se ho rubini o topazi in eccesso li modifico e aggiorno la nuova dimensione massima
     int r,t;
     r = restanti[1]; t = restanti[2];
@@ -197,7 +213,7 @@ void riduciPietreNonValide(int *restanti, int *k){
         r = t+1;
     if (t > 1+r)
         t = r+1;
-    *k = restanti[0]+restanti[3]+r+t;
+    *k = restanti[0]+restanti[3]+r+t; //aggiornamento dimensione
     restanti[1] = r; restanti[2] = t;
 }
 
