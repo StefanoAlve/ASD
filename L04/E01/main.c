ef1fe99f07c213c13fcce6a2acb81b27e85dd6f8
Gianbattistabsn <gianbattista.busonera@studenti.polito.it>
2023-11-09 15:47:03 +0100

    L4E3 non ottimizzato

--- L04/E01/main.c
+++ L04/E01/main.c
@@ -1,6 +1,133 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    printf("Esercizio 1 L04\n");
+#define file_name "grafo.txt"
+typedef struct{
+    int u;
+    int v;
+}arco;
+
+void riempi_vett_archi(FILE *fp_in, arco* *vet_archi, int* N, int* E);
+void stampa_vett_archi(arco *vet_archi, int E);
+int* vet_vertici(int N);
+int powerset_mod(int pos, int *val, int *sol, int k, int count, int *vertex_cover, arco *archi, int E);
+int check_vertex_cover(int *vertex_cover, arco *archi, int N, int E, int count);
+
+int main()
+{
+    arco *vet_archi;
+    int *sol, *vertex_cover;
+    int cont = 0;
+    int N, E, *vertici = NULL;
+    FILE *fp_in = fopen(file_name, "r");
+    riempi_vett_archi(fp_in, &vet_archi, &N, &E);
+    stampa_vett_archi(vet_archi, E);
+    vertici = vet_vertici(N);
+    sol = (int*)malloc(N*sizeof(int));
+    vertex_cover = (int*)malloc(N * sizeof(int));
+    printf("\n");
+    cont = powerset_mod(0, vertici, sol, N, cont, vertex_cover, vet_archi, E);
+    if (cont)
+        printf("\nIn totale ci sono %d vertex cover.", cont);
+    else
+        printf("\nNon ho trovato alcun vertex cover.");
+    free(vet_archi);
+    free(sol);
+    free(vertex_cover);
+    free(vertici);
+    fclose(fp_in);
     return 0;
+}
+
+
+void riempi_vett_archi(FILE *fp_in, arco* *vet_archi, int* N, int* E)
+{
+    int n_vertici, n_archi, i = 0;
+    arco *vet_archi_t = NULL;
+    fscanf(fp_in, "%d %d", &n_vertici, &n_archi);
+    vet_archi_t = (arco*)malloc(n_archi*sizeof(arco));
+    while(fscanf(fp_in, "%d %d", &(vet_archi_t[i].u), &(vet_archi_t[i].v)) == 2)
+        i++;
+    *N = n_vertici;
+    *vet_archi = vet_archi_t;
+    *E = n_archi;
+}
+
+void stampa_vett_archi(arco *vet_archi, int E)
+{
+    int i;
+    printf("\nHo trovato i seguenti archi:");
+    for(i = 0; i < E; i++){
+        printf(" (%d-%d)", vet_archi[i].u, vet_archi[i].v);
+    }
+}
+
+int* vet_vertici(int N)
+{
+    int *vertici = (int*)malloc(N*sizeof(int));
+    int i;
+    printf("\n\nI vertici del grafo sono:\n");
+    for (i = 0; i < N; i++){
+        vertici[i] = i;
+    }
+    for(i = 0; i < N; i++)
+        printf("%d ", vertici[i]);
+    printf("\n");
+    return vertici;
+}
+
+int powerset_mod(int pos, int *val, int *sol, int k, int count, int *vertex_cover, arco *archi, int E)
+{
+    int i;
+    if (pos >= k)
+    {
+        for(i = 0; i < k; i++)
+        {
+            if (sol[i] != 0)
+                vertex_cover[i] = val[i];
+            else
+                vertex_cover[i] = -1;
+        }
+        count += check_vertex_cover(vertex_cover, archi, k, E, count);
+        return count;
+    }
+    sol[pos] = 0;
+    count = powerset_mod(pos+1, val, sol, k, count, vertex_cover, archi, E);
+    sol[pos] = 1;
+    count = powerset_mod(pos+1, val, sol, k, count, vertex_cover, archi, E);
+    return count;
+}
+
+
+int check_vertex_cover(int *vertex_cover, arco *archi, int n, int e, int count)
+{
+    int i, j, u, v, flag1 = 0, flag2 = 1, valore;
+    for (i = 0; i < e; i++)
+    {
+        flag1 = 0;
+        u = archi[i].u;
+        v = archi[i].v;
+        for (j = 0; (j < n) & !flag1; j++)
+        {
+            if (vertex_cover[j] != -1)
+            {
+                valore = vertex_cover[j];
+                if (valore >= 0 && (valore == u || valore == v))
+                    flag1 = 1; // ho trovato un vertice che incide su uno dei due vertici dell'arco (u,v)
+                else
+                    flag1 = 0;
+            }
+        }
+        if(flag1 == 0)
+            flag2 = 0;
+    }
+    if(flag2)
+    {
+        printf("Vertex cover %d:\n", count+1);
+        for(i = 0; i < n; i++)
+            if(vertex_cover[i] != -1)
+                printf("%d\t", vertex_cover[i]);
+        printf("\n");
+    }
+    return flag2;
 }
--- L04/E03/main.c
+++ L04/E03/main.c
@@ -1,6 +1,142 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define file_name "easy_test_set.txt"
+#define num_pietre_distinte 4
+typedef struct{
+    int z;
+    int r;
+    int t;
+    int s;
+    int maxl;
+}pietre;
 
-int main() {
-    printf("Esercizio 3 L04\n");
+void leggi_file(pietre* *collane, int* nTest);
+void stampa_collane(pietre *collane, int nCollane);
+void inizializza_mark(int* mark, pietre collana);
+int disp_rip(char *val, char* sol, int mark[4], int n_pietre_tot, int n_dist, int k, int pos, int flag, int start);
+int isValid(char *sol, int k, int pos);
+
+int main(void)
+{
+    int nTest, k, n, n_dist = num_pietre_distinte, fine = 0, i, mark[num_pietre_distinte], start = 0;
+    char *sol, val[4];
+    pietre *collane;
+    leggi_file(&collane, &nTest);
+    printf("Il file contiene i seguenti %d test da effettuare:\n\n", nTest);
+    stampa_collane(collane, nTest);
+    val[0] = 'z'; val[1] = 'r'; val[2] = 't'; val[3] = 's';
+    for (i = 0; i < nTest; i++)
+    {
+        fine = 0;
+        printf("TEST#%d:", i+1);
+        n = k = collane[i].maxl;  // il numero n di pietre totali inizialmente è uguale a k (posso disporle in k modi diversi)
+        inizializza_mark(mark, collane[i]);   //mark conterrà quante volte, al massimo, posso usare le determinate pietre dell'i-esima collana
+        printf("\n");
+        while(!fine)
+        {
+            sol = (char *)malloc(k * sizeof(char));
+            fine = disp_rip(val, sol, mark, n, n_dist, k, 0, 0, start);
+            if(fine)
+                printf("Sono stati usati %d zaffiri, %d rubini, %d topazi, %d smeraldi\n\n", mark[0], mark[1], mark[2], mark[3]);
+            k--;
+            free(sol);
+        }
+
+    }
     return 0;
+}
+
+
+void leggi_file(pietre* *collane, int* nTest)
+{
+    FILE* fp_in = fopen(file_name, "r");
+    pietre *tmp = NULL;
+    int i = 0;
+    if(fp_in != NULL)
+    {
+        fscanf(fp_in, "%d",  nTest);
+        tmp = (pietre *)malloc(*nTest * sizeof(pietre));
+        while(fscanf(fp_in, " %d %d %d %d", &tmp[i].z, &tmp[i].r, &tmp[i].t, &tmp[i].s) == 4)
+        {
+            tmp[i].maxl = tmp[i].z+tmp[i].r+tmp[i].t+tmp[i].s;
+            i++;
+        }
+    }
+    else
+        printf("Errore nell'apertura del file di input.\n");
+    fclose(fp_in);
+    *collane = tmp;
+}
+
+void stampa_collane(pietre *collane, int nCollane)
+{
+    int i;
+    for (i = 0; i < nCollane; i++){
+        printf("TEST #%d: %d zaffiri, %d rubini, %d topazi, %d smeraldi\nNumero pietre totali:%d\n", i+1, collane[i].z, collane[i].r, collane[i].t, collane[i].s, collane[i].maxl);
+    }
+}
+
+
+int disp_rip(char *val, char* sol, int mark[4], int n_pietre_tot, int n_dist, int k, int pos, int flag, int start)
+{
+    int i;
+    if(pos >= k)
+    {
+        printf("\nLa collana che originariamente disponeva di %d pietre, puo' essere riempita con %d pietre:\n",n_pietre_tot, k);
+
+        for(i = 0; i < k; i++)
+        {
+            printf("%c", sol[i]);
+        }
+        printf("\n");
+        flag = 1;
+        return flag; // cont sarà uguale a 1; significa che ne ho trovata 1
+    }
+    for(i = start; i < n_dist && !flag; i++) // i partità dallo start (lo start indica che pietra prendere prima)
+    {
+        if(mark[i]>0)
+        {
+            mark[i]--;
+            sol[pos] = val[i];
+            if (isValid(sol, k, pos)) // se la configurazione della soluzione (fino all'indice pos) è valida allora ricorro, altrimenti passo alla pietra successiva
+            {
+                flag = disp_rip(val, sol, mark, n_pietre_tot, n_dist, k, pos + 1, flag, 0);
+            }
+            mark[i]++;
+        }
+        if (flag) // è inutile continuare a ricorrere perchè ho trovato una configurazione valida
+            return flag;
+    }
+    return flag;
+}
+
+int isValid(char *sol, int k, int pos)
+{
+    int valid = 0;
+    char c_pietra, p_pietra;
+    if (pos == 0) // se è il primo elemento è valida qualsiasi sia la pietra
+        valid =1;
+    else if(pos>0 && pos < k) /// se invece non è il primo elemento della soluzione confronto con il precedente e verifico validità:
+    {
+        c_pietra = sol[pos];
+        p_pietra = sol[pos - 1];
+        if(c_pietra == 'z' && (p_pietra == 'z' || p_pietra == 't'))
+            valid = 1;
+        else if(c_pietra == 's' && (p_pietra == 's' || p_pietra == 'r'))
+            valid = 1;
+        else if(c_pietra == 'r' && (p_pietra == 'z' || p_pietra == 't'))
+            valid = 1;
+        else if(c_pietra == 't' && (p_pietra == 'r' || p_pietra == 's'))
+            valid = 1;
+        else
+            valid = 0;
+    }
+    return valid;
+}
+
+void inizializza_mark(int* mark, pietre collana){
+    mark[0] = collana.z;
+    mark[1] = collana.r;
+    mark[2] = collana.t;
+    mark[3] = collana.s;
 }
