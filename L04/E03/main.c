7bd608210918c235f4cfaad3e72c6fc37522d891
sgabelli <s308759@studenti.polito.it>
2023-11-10 13:28:42 +0100

    riempiti i file .txt

--- L04/E03/main.c
+++ L04/E03/main.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAXL 50
+
+typedef struct {
+    char pietra[MAXL];
+    int numero;
+}stones;
+
+int leggiFile(char nomeFile[MAXL], stones **pietre);
 
 int main() {
-    printf("Esercizio 3 L04\n");
+    char nomeFile[MAXL];
+    stones *pietre;
+    int nr;
+
+    printf("inserire nome file: \n");
+    scanf("%s", nomeFile);
+
+    nr = leggiFile(nomeFile, &pietre);
+
     return 0;
+}
+
+
+int leggiFile(char nomeFile[MAXL], stones **pietre) {
+
 }
