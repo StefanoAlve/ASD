0880d7875d0ab77a3c3f901624f6654acad13dc2
sgabelli <s308759@studenti.polito.it>
2023-11-14 22:20:27 +0100

    codice finito e debuggato

--- L04/E03/main.c
+++ L04/E03/main.c
@@ -2,45 +2,37 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAXL 50
-
+#define numPietre 4
 typedef struct {
-    int numero[4];
+    int numero[numPietre];
 }stones;
 
 
 stones *leggiFile(char nomeFile[MAXL], stones *pietre, int *nr);
-void collane(stones *pietre, int nr);
-int maxCompl(stones *pietre, int riga);
-int maxTraDue(stones *pietre, int i, int j, int riga);
 void stampaPietre(stones *pietre, int riga);
-void stampaCollana(int collana[4], int tot);
+void stampaCollana(char *collana, int dimensione);
+void preparativo(int nr, stones *pietre);
+void rimanenti(int riman[numPietre], stones pietre);
+int trovaDimensione(int riman[numPietre], int dimensione);
+int formaCollana(int riman[numPietre], int dimensione, char *collana, int bool, int pos);
+int condizione(char *collana, int pos);
 
 int main() {
     char nomeFile[MAXL];
     stones *pietre = NULL;
-    int nr, i, j;
+    int nr;
 
     printf("inserire nome file: \n");
     scanf("%s", nomeFile);
 
     pietre = leggiFile(nomeFile, pietre, &nr);
-
-    /*for(i=0; i<nr; i++){
-        printf("gruppo %d: ", i+1);
-        for(j=0; j<4; j++){
-            printf(" %d", pietre[i].numero[j]);
-        }
-        printf("\n");
-    }*/
-
-    collane(pietre, nr);
+    preparativo(nr, pietre);
 
     free(pietre);
 
     return 0;
 }
-
-
+///funzione che legge il file
 stones *leggiFile(char nomeFile[MAXL], stones *pietre, int *nr) {
     FILE *fp;
     int i, j;
@@ -68,116 +60,14 @@ stones *leggiFile(char nomeFile[MAXL], stones *pietre, int *nr) {
     fclose(fp);
     return pietre;
 }
-
-void collane(stones *pietre, int nr) {
-    int collana[4], i, j, indexMax, flag = 1, index, z=0, r=1, t=2, s=3, cont=0;
-    ///CICLO CHE DOVRA FARE LE COLLANE
-    for(i=0; i<nr; i++) {
-        ///INIZIALIZZO OGNI VOLTA GLI ELEMENTI DELLE COLLANE A 0
-        for(j=0; j<4; j++) {
-            collana[j] = 0;
-        }
-        ///FUNZIONE DI STAMPA PIETRE
-        stampaPietre(pietre, i);
-        ///ELEMENTO DA CUI PARTIRE
-        indexMax = maxCompl(pietre, i);
-
-
-
-        pietre[i].numero[indexMax] -= 1;
-        collana[indexMax] += 1;
-        cont++;
-        index = indexMax;
-
-
-        while (flag) {
-            ///CASO ZAFFIRO E TOPAZIO
-            if (index == z || index == t) {
-                ///CONDIZIONE TERMINATRICE
-                if (pietre[i].numero[z] == 0 && pietre[i].numero[r] == 0) {
-                    //if (pietre[i].numero[s] == 0 || pietre[i].numero[t] == 0)
-                    flag = 0;
-
-                } else {
-                    if (pietre[i].numero[s] == 0 && pietre[i].numero[t] == 0) {
-                        index = z;
-                        pietre[i].numero[index] -= 1;
-                        collana[index] += 1;
-                        cont++;
-                        if (pietre[i].numero[z] == 0)
-                            flag = 0;
-                    } else {
-                        index = maxTraDue(pietre, z, r, i);
-                        pietre[i].numero[index] -= 1;
-                        collana[index] += 1;
-                        cont++;
-                    }
-                }
-            }
-                ///CASO SMERALDO E RUBINO
-            else if (index == s || index == r) {
-                ///CONDIZIONE TERMINATRICE
-                if (pietre[i].numero[s] == 0 &&
-                    pietre[i].numero[t] == 0 /*&& (pietre[i].numero[z] == 0 || pietre[i].numero[r] == 0)*/)
-                    flag = 0;
-                    ///DECREMENTO PIETRE E INCREMENTO COLLANA
-                else {
-                    if (pietre[i].numero[z] == 0 && pietre[i].numero[r] == 0) {
-                        index = s;
-                        pietre[i].numero[index] -= 1;
-                        collana[index] += 1;
-                        cont++;
-                        if (pietre[i].numero[s] == 0)
-                            flag = 0;
-                    } else {
-                        index = maxTraDue(pietre, s, t, i);
-                        pietre[i].numero[index] -= 1;
-                        collana[index] += 1;
-                        cont++;
-                    }
-
-                }
-            }
-        }
-        ///FUNZIONE DI STAMPA COLLANA
-        stampaCollana(collana, cont);
-        flag = 1;
-        cont = 0;
-
-
-    }
-}
-
-///TROVA L'INDICE CON VALORE DI GRANDEZZA MASSIMA TRA TUTTI
-int maxCompl(stones *pietre, int riga) {
-    int i, ind=0;
-
-    for(i=0; i<4; i++){
-        if (pietre[riga].numero[i] > pietre[riga].numero[ind]) {
-            ind = i;
-        }
-    }
-
-    return ind;
-}
-
-///TROVA L'INDICE CON IL VALORE MASSIMO TRA DUE
-int maxTraDue(stones *pietre, int i, int j, int riga) {
-    int max = i;
-
-    if(pietre[riga].numero[j] > pietre[riga].numero[i])
-        max = j;
-
-    return max;
-}
-
 ///STAMPA LA COLLANA
-void stampaCollana(int collana[4], int tot) {
-
-    printf("COLLANA MASSIMA\nzaffiro = %d, rubino = %d,  topazio = %d, smeraldo = %d, TOT COLLANA = %d\n\n", collana[0],
-           collana[1], collana[2], collana[3], tot);
+void stampaCollana(char *collana, int dimensione) {
+    int i;
+    for(i=0; i<dimensione; i++){
+        printf("%c ", collana[i]);
+    }
+    printf("\n\n");
 }
-
 ///STAMPA LE PIETRE
 void stampaPietre(stones *pietre, int riga){
     int tot=pietre[riga].numero[0] + pietre[riga].numero[1] + pietre[riga].numero[2] + pietre[riga].numero[3];
@@ -185,4 +75,108 @@ void stampaPietre(stones *pietre, int riga){
     printf("TEST #%d\n", riga+1);
     printf("zaffiro = %d, rubino = %d,  topazio = %d, smeraldo = %d, TOT = %d\n", pietre[riga].numero[0], pietre[riga].numero[1],
            pietre[riga].numero[2], pietre[riga].numero[3], tot);
+}
+///vettore temporaneo collana
+void rimanenti(int riman[numPietre], stones pietre){
+    riman[0] = pietre.numero[0];
+    riman[1] = pietre.numero[1];
+    riman[2] = pietre.numero[2];
+    riman[3] = pietre.numero[3];
+}
+///dimensione delle soluzioni
+int trovaDimensione(int riman[numPietre], int dim) {
+    int dimensione;
+    if(riman[1] > riman[2])
+        dimensione = riman[0] + riman[3] + (riman[1] + riman[2]) - (riman[1] - riman[2]) + 1;
+    else if(riman[1] < riman[2])
+        dimensione = riman[0] + riman[3] + (riman[2] + riman[1]) - (riman[2] - riman[1]) + 1;
+    else
+        dimensione = dim;
+
+    return dimensione;
+}
+///FUNZIONE WRAP CHE RACCOGLIE LA MAGGIOR PARTE DELLE OPERAZIONI PER FARE LA COLLANA
+void preparativo(int nr, stones *pietre){
+    int riman[numPietre], bool=0, i, dimensione, dimensioneMAX;
+    char *collana;
+
+    for(i=0; i<nr; i++) {
+        stampaPietre(pietre, i);
+        rimanenti(riman, pietre[i]);
+        dimensioneMAX = riman[0] + riman[1] + riman[2] + riman[3];
+        dimensione = trovaDimensione(riman, dimensioneMAX);
+        ///ALLOCO OGNI VOLTA LA COLLANA DELLA LUNGHEZZA CHE MI SERVE
+        collana = malloc(dimensione*sizeof(char));
+        if(collana == NULL){
+            printf("errore nell'allocazione");
+            exit(1);
+        }
+
+        formaCollana(riman, dimensione, collana, bool, 0);
+    }
+
+    free(collana);
+}
+///FUNZIONE CHE FORMA LA COLLANA IN MANIERA RICORSIVA
+int formaCollana(int riman[numPietre], int dimensione, char *collana, int bool, int pos) {
+    int i;
+
+    ///CONDIZIONE DI TERMINAZIONE
+    if(pos >= dimensione) {
+        printf("La collana e' lunga %d pietre :\n", dimensione);
+        stampaCollana(collana, dimensione);
+        bool = 1;
+        return bool;
+    }
+    ///PASSO SU OGNI PIETRA PARTENDO DALLA PRIMA FINCHE NON TROVO QUELLA APPARTENENTE ALLA CATENA GIUSTA
+    for(i=0; i<numPietre; i++){
+        if(riman[i] > 0){
+            riman[i]--;
+            ///RIEMPIO LA COLLANA
+            if(i == 0){
+                collana[pos] = 'z';
+            }
+            else if(i == 1){
+                collana[pos] = 'r';
+            }
+            else if(i == 2){
+                collana[pos] = 't';
+            }
+            else if(i == 3){
+                collana[pos] = 's';
+            }
+            ///CONTROLLO CHE LA COLLANA RISPETTI LE REGOLE
+            if(condizione(collana, pos)){
+                bool = formaCollana(riman, dimensione, collana, bool, pos+1);
+            }
+            riman[i]++;
+            if(bool){
+                return bool;
+            }
+        }
+    }
+    return bool;
+
+}
+///CONTROLLA CHE LA LEGGE DI FORMAZIONE DELLA COLLANA SIA RISPETTATA
+int condizione(char *collana, int pos) {
+    int val = 1;
+
+    if (pos == 0)
+        return val;
+    if(collana[pos -1] == 'z')
+        if(collana[pos] == 'z' || collana[pos] == 'r')
+            return val;
+    if(collana[pos -1] == 'r')
+        if(collana[pos] == 's' || collana[pos] == 't')
+            return val;
+    if(collana[pos -1] == 't')
+        if(collana[pos] == 'z' || collana[pos] == 'r')
+            return val;
+    if(collana[pos -1] == 's')
+        if(collana[pos] == 's' || collana[pos] == 't')
+            return val;
+
+    val = 0;
+    return val;
 }
