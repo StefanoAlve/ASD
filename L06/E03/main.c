0dba1d2e030a7aa57edd1e4f0f24d9e9eed25064
sgabelli <s308759@studenti.polito.it>
2023-11-30 13:04:41 +0100

    aggiunta di qualche funzione

--- L06/E03/main.c
+++ L06/E03/main.c
@@ -4,11 +4,21 @@
 #include "personaggi.c"
 int main() {
     char filePg[MAXC] = "pg.txt";
-    pnodoPg_t x;
+    pnodoPg_t x, y;
     ptabPg tabPg = (ptabPg)malloc(sizeof(*tabPg));
-    tabPg = leggiPersonaggi(filePg);
+    struct pg_t personaggio;
 
-    for(x = tabPg->headPg; x->next != NULL; x = x->next){
+    tabPg = leggiPersonaggi(filePg);
+    //PG0001 Sephiroth Guerriero 1832 71 116 65 41 49
+    strcpy(personaggio.codice, "PG0011");
+    strcpy(personaggio.nome, "PG0011");
+    strcpy(personaggio.classe, "PG0011");
+
+    inserisciPg(tabPg, personaggio);
+    y = &personaggio;
+    cancellaPg(tabPg, y);
+
+    for(x = tabPg->headPg; x != NULL; x = x->next){
         printf("%s\n", x->codice);
     }
 
--- L06/E03/personaggi.c
+++ L06/E03/personaggi.c
@@ -28,12 +28,13 @@ ptabPg leggiPersonaggi(char *nomeFile) {
         printf("Errore nell'apertura del file personaggi!\n");
         exit(1);
     }
+    //continua per tutto il file
     while(!feof(fin)) {
         fscanf(fin, "%s %s %s", personaggio.codice, personaggio.nome, personaggio.classe);
         fscanf(fin, "%d %d %d %d %d %d", &personaggio.stat.hp, &personaggio.stat.mp, &personaggio.stat.atk, &personaggio.stat.def, &personaggio.stat.mag, &personaggio.stat.spr);
         personaggio.next = NULL;
         tabPg->headPg = newNode(tabPg->headPg, personaggio);
-        tabPg->nPg += 1;
+        tabPg->nPg += 1;        //aumenta il numero di personaggi
         tabPg->tailPg = &personaggio;
     }
 
@@ -41,19 +42,82 @@ ptabPg leggiPersonaggi(char *nomeFile) {
     fclose(fin);
     return tabPg;
 }
-
+//funzione che genera un nuovo nodo nella lista di personaggi
 pnodoPg_t newNode(pnodoPg_t head, struct pg_t personaggio) {
     pnodoPg_t x, newNode = malloc(sizeof( *newNode));
-
+    //creo newnode per far si che non si creino problemi con la variabile personaggio
     *newNode = personaggio;
     newNode->next = NULL;
 
     if(head == NULL){
-        head = newNode;
+        head = newNode;     //inserimento in testa
     }
     else {
         for (x = head; x->next != NULL; x = x->next) {}
         x->next = newNode;
     }
     return head;
+}
+//funzione che inserisce un personaggio preso da tastiera
+ptabPg inserisciPg(ptabPg tabPg, struct pg_t personaggio) {
+    personaggio.next = NULL;
+    tabPg->headPg = newNode(tabPg->headPg, personaggio);
+    tabPg->nPg += 1;
+    return tabPg;
+}
+//funzione che cancella un personaggio
+ptabPg cancellaPg(ptabPg tabPg, pnodoPg_t personaggio) {
+    pnodoPg_t x, p;
+    for (x = tabPg->headPg, p=NULL; x != NULL ; p=x, x = x->next) {
+        if(strcmp(personaggio->codice, x->codice)==0){
+            if(x == tabPg->headPg){
+                tabPg->headPg = tabPg->headPg->next;
+            }
+            else {
+                p->next = x->next;
+            }
+            free(x);
+            break;
+        }
+    }
+
+    return tabPg;
+}
+//funzione che ricerca per codice
+pnodoPg_t ricercaCodice(ptabPg tabPg, char nome[]) {
+    pnodoPg_t x;
+    for (x = tabPg->headPg; x != NULL; x = x->next) {
+        if (strcmp(nome, x->codice) == 0) {
+            return x;
+        }
+    }
+    return NULL;
+}
+void stampaPgCode(pnodoPg_t personaggio){
+    printf("%s\t", personaggio->codice);
+}
+void stampaPgNome(pnodoPg_t personaggio){
+    printf("%s\t", personaggio->nome);
+}
+void stampaPgClasse(pnodoPg_t personaggio){
+    printf("%s\t", personaggio->classe);
+}
+void stampaPgEquip(pnodoPg_t personaggio, ptabInv tabInv) {
+    int i;
+    printf("equipaggiamento: ");
+    for(i=0; i<MAXOBJ; i++){
+        if(personaggio->equip->vettEq[i] == -1){
+            break;
+        }
+        else{
+            stampaObjNome(tabInv, personaggio->equip->vettEq[i]);
+        }
+    }
+}
+void stampaPgStat(pnodoPg_t personaggio) {
+    printf("%d, %d, %d, %d, %d, %d\n\n", personaggio->stat.hp, personaggio->stat.mp, personaggio->stat.atk, personaggio->stat.def, personaggio->stat.mag, personaggio->stat.spr);
+}
+//stampa personaggio
+void stampaPg(pnodoPg_t personaggio, ptabInv tabInv){
+
 }
