1ccf59fba946cadd622b0213bb22f52bec118a9b
Gianbattistabsn <gianbattista.busonera@studenti.polito.it>
2023-12-02 11:06:49 +0100

    L7E1 finito

---
+++ L07/E01/cmake-build-debug/CMakeFiles/E01.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/gianb/Desktop/UNI/ASD/ASD_LAIB/L07/E01/equipArray.c" "CMakeFiles/E01.dir/equipArray.c.obj" "gcc" "CMakeFiles/E01.dir/equipArray.c.obj.d"
+  "C:/Users/gianb/Desktop/UNI/ASD/ASD_LAIB/L07/E01/inv.c" "CMakeFiles/E01.dir/inv.c.obj" "gcc" "CMakeFiles/E01.dir/inv.c.obj.d"
+  "C:/Users/gianb/Desktop/UNI/ASD/ASD_LAIB/L07/E01/invArray.c" "CMakeFiles/E01.dir/invArray.c.obj" "gcc" "CMakeFiles/E01.dir/invArray.c.obj.d"
+  "C:/Users/gianb/Desktop/UNI/ASD/ASD_LAIB/L07/E01/main.c" "CMakeFiles/E01.dir/main.c.obj" "gcc" "CMakeFiles/E01.dir/main.c.obj.d"
+  "C:/Users/gianb/Desktop/UNI/ASD/ASD_LAIB/L07/E01/pg.c" "CMakeFiles/E01.dir/pg.c.obj" "gcc" "CMakeFiles/E01.dir/pg.c.obj.d"
+  "C:/Users/gianb/Desktop/UNI/ASD/ASD_LAIB/L07/E01/pgList.c" "CMakeFiles/E01.dir/pgList.c.obj" "gcc" "CMakeFiles/E01.dir/pgList.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ L07/E01/cmake-build-debug/CMakeFiles/E01.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/E01.dir/equipArray.c.obj"
+  "CMakeFiles/E01.dir/equipArray.c.obj.d"
+  "CMakeFiles/E01.dir/inv.c.obj"
+  "CMakeFiles/E01.dir/inv.c.obj.d"
+  "CMakeFiles/E01.dir/invArray.c.obj"
+  "CMakeFiles/E01.dir/invArray.c.obj.d"
+  "CMakeFiles/E01.dir/main.c.obj"
+  "CMakeFiles/E01.dir/main.c.obj.d"
+  "CMakeFiles/E01.dir/pg.c.obj"
+  "CMakeFiles/E01.dir/pg.c.obj.d"
+  "CMakeFiles/E01.dir/pgList.c.obj"
+  "CMakeFiles/E01.dir/pgList.c.obj.d"
+  "E01.exe"
+  "E01.exe.manifest"
+  "E01.pdb"
+  "libE01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/E01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
