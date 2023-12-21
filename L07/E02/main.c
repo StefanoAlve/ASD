f123d3805f42cfe1881ff67a57925ada4591fd8c
sgabelli <s308759@studenti.polito.it>
2023-12-21 10:22:39 +0100

    base finita

--- L07/E02/main.c
+++ L07/E02/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #define MAXC 100
+#define MAXELEM 5
 
 typedef struct{
     char nome[MAXC];
@@ -14,8 +15,23 @@ typedef struct{
     int difficolta;
 }elementi_s;
 
+typedef struct {
+    int nDiag;
+    int nGiri;
+    int acrobatico;
+    int acrAvan;
+    int acrInd;
+    int acrDiFila;
+    int moltiplicatore;
+    int diffTot;
+    int diffDiag;
+}regole_s;
+
 int leggiFile(char nomeFile[MAXC], elementi_s **elementi);
 void wrapper(elementi_s *elementi, int nr);
+elementi_s diagonaliR(int DD, int DP, int nrSet, elementi_s *set, elementi_s *definitivo, elementi_s *tmp, regole_s regole, int pos);
+elementi_s copia(elementi_s primo, elementi_s secondo);
+regole_s aggiornaRegole(regole_s regole, elementi_s tmp);
 
 int main() {
     char nomeFile[MAXC] = "elementi.txt";
@@ -62,7 +78,8 @@ int leggiFile(char nomeFile[MAXC], elementi_s **elementi) {
 void wrapper(elementi_s *elementi, int nr) {
     int DD, DP, nrSet, bool=1, i, index;
     char decisione[MAXC];
-    elementi_s set[nr];
+    elementi_s set[nr], definitivo[nr], tmp[nr];
+    regole_s regole;
 
     while(bool) {
         printf("\n\n\n decidi quanti elementi vuoi nel set(max %d)\n", nr);
@@ -86,10 +103,15 @@ void wrapper(elementi_s *elementi, int nr) {
         printf("scegli il limite di difficolta' di diagonale e di programma\n");
         scanf("%d %d", &DD, &DP);
 
-
-
-
-
+        regole.nDiag = 0;
+        regole.nGiri = 0;
+        regole.acrobatico = 0;
+        regole.acrAvan = 0;
+        regole.acrInd = 0;
+        regole.acrDiFila = 0;
+        regole.moltiplicatore = 0;
+        regole.diffTot = 0;
+        regole.diffDiag = 0;
 
         printf("vuoi continuare si o no?");
         scanf("%s", decisione);
@@ -97,4 +119,43 @@ void wrapper(elementi_s *elementi, int nr) {
             bool = 0;
         }
     }
+}
+
+
+elementi_s diagonaliR(int DD, int DP, int nrSet, elementi_s *set, elementi_s *definitivo, elementi_s *tmp, regole_s regole, int pos) {
+    int i;
+
+    for(i=0; i<nrSet; i++) {
+        if(regole.nDiag == 0){
+            if(set[i].reqDiPrec == 0 && set[i].difficolta <= DD && (set[i].difficolta + regole.diffTot) <= DP){
+
+                tmp[pos] = copia(tmp[pos], set[i]);
+
+                definitivo = diagonaliR(DD, DP)
+            }
+        }
+
+
+    }
+
+
+}
+
+regole_s aggiornaRegole(regole_s regole, elementi_s tmp){
+    regole.nDiag += 1;
+    if (tmp.tipologia != 0){
+        regole
+    }
+}
+
+elementi_s copia(elementi_s primo, elementi_s secondo) {
+    strcpy(primo.nome, secondo.nome);
+    primo.tipologia = secondo.tipologia;
+    primo.dirIng = secondo.dirIng;
+    primo.dirUsc = secondo.dirUsc;
+    primo.reqDiPrec = secondo.tipologia;
+    primo.finale = secondo.finale;
+    primo.valore = secondo.valore;
+    primo.difficolta = secondo.difficolta;
+    return primo;
 }
