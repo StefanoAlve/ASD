278005f10916d1d957b0ee7fecb71a5f30061cd3
Stefano <s312200@studenti.polito.it>
2024-01-21 20:25:56 +0100

    test

---
+++ L06/E03/cmake-build-debug/CMakeFiles/E03.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/ASD/L06/E03/client.c" "CMakeFiles/E03.dir/client.c.obj" "gcc" "CMakeFiles/E03.dir/client.c.obj.d"
+  "C:/ASD/L06/E03/inv.c" "CMakeFiles/E03.dir/inv.c.obj" "gcc" "CMakeFiles/E03.dir/inv.c.obj.d"
+  "C:/ASD/L06/E03/main.c" "CMakeFiles/E03.dir/main.c.obj" "gcc" "CMakeFiles/E03.dir/main.c.obj.d"
+  "C:/ASD/L06/E03/pg.c" "CMakeFiles/E03.dir/pg.c.obj" "gcc" "CMakeFiles/E03.dir/pg.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
