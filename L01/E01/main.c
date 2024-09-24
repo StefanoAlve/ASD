dd0d8caeee84b89b065077380d4043b5903fb612
AlessoBenvegna <147596860+AlessoBenvegna@users.noreply.github.com>
2024-09-24 15:35:45 +0200

    funzione regexp completata

--- L01/E01/main.c
+++ L01/E01/main.c
@@ -3,11 +3,12 @@
 #include <ctype.h>
 #include <stdlib.h>
 
+int CercaLettera(char *r, char c);
 char *cercaRegexp(char *src, char *regexp);
 
 int main() {
     char str[] = "Pippoflauto";
-    char Rg[] = "A[^f]\\anR.d";
+    char Rg[] = "P[^f]\\anR.d";
     char *p = cercaRegexp(str, Rg);
 
     while (*p != '\0'){
@@ -18,5 +19,32 @@ int main() {
 }
 
 char *cercaRegexp(char *src, char *regexp){
+    int found = 0, appartenenza;
+    char *s = src;
+    while (*s != '\0'){
+        char *rg = regexp;
+        while(*rg != '\0' && !found){
+            if (*rg == '['){
+                appartenenza = CercaLettera(rg, *s);
+                if (*(rg+1) == '^'){
+                    if (appartenenza) found = 1;
+                }
+                else if (!appartenenza) found = 1;
+            } else if (*rg == '\\'){
+                rg++;
+                if ((*rg == 'a' && isupper(*s)) || (*rg == 'A' && islower(*s))) found = 1;
+            } else if (*rg != '.' && *rg != *s){
+                found = 1;
+            }
+            s++; rg++;
+        }
+        if (!found) return src;
+        src++; s = src;
+    }
+    return NULL;
+
+}
+
+int CercaLettera(char *r, char c){
 
 }
