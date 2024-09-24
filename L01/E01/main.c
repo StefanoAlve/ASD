add31255e33e8445f4a625554594e710271918e6
AlessoBenvegna <147596860+AlessoBenvegna@users.noreply.github.com>
2024-09-24 16:00:40 +0200

    es completato, da controllare

--- L01/E01/main.c
+++ L01/E01/main.c
@@ -7,21 +7,23 @@ int CercaLettera(char *r, char c);
 char *cercaRegexp(char *src, char *regexp);
 
 int main() {
-    char str[] = "Pippoflauto";
-    char Rg[] = "P[^f]\\anR.d";
+    char str[] = "PipPoflauto";
+    char Rg[] = "a.[rftg]\\a";
     char *p = cercaRegexp(str, Rg);
-
-    while (*p != '\0'){
-        printf("%c", *p++);
+    if (p != NULL){
+        while (*p != '\0'){
+            printf("%c", *p++);
+        }
     }
 
     return 0;
 }
 
 char *cercaRegexp(char *src, char *regexp){
-    int found = 0, appartenenza;
+    int appartenenza;
     char *s = src;
     while (*s != '\0'){
+        int found = 0;
         char *rg = regexp;
         while(*rg != '\0' && !found){
             if (*rg == '['){
@@ -30,6 +32,7 @@ char *cercaRegexp(char *src, char *regexp){
                     if (appartenenza) found = 1;
                 }
                 else if (!appartenenza) found = 1;
+                while(*rg != ']') rg++;
             } else if (*rg == '\\'){
                 rg++;
                 if ((*rg == 'a' && isupper(*s)) || (*rg == 'A' && islower(*s))) found = 1;
@@ -46,5 +49,11 @@ char *cercaRegexp(char *src, char *regexp){
 }
 
 int CercaLettera(char *r, char c){
-
+    int find = 0;
+    char *pr = r;
+    while(!isalpha(*pr)) pr++;
+    while(*pr != ']'){
+        if (*pr++ == c) find = 1;
+    }
+    return find;
 }
