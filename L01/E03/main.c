06de4db258ff7c254dcf24436b748b80fdd08e95
AlessoBenvegna <147596860+AlessoBenvegna@users.noreply.github.com>
2024-09-25 15:45:41 +0200

    es completo

--- L01/E03/main.c
+++ L01/E03/main.c
@@ -21,6 +21,14 @@ typedef struct{
 comando_e StampaMenu();
 int LeggiFile(FILE *fp, s_tratta tratte[MAXN]);
 void SelezionaFunzione(comando_e comando, int n, int *pfine, s_tratta Tratte[]);
+void StampaCorse(int n, s_tratta Tratte[]);
+void SortDate(int n, s_tratta Tratte[]);
+void SortCod(int n, s_tratta Tratte[]);
+void SortPart(int n, s_tratta Tratte[]);
+void SortDest(int n, s_tratta Tratte[]);
+void RicercaTratta(int n, s_tratta Tratte[]);
+void linearSearch(int n, s_tratta Tratte[], char str[MAXN]);
+void binarySearch(int n, s_tratta Tratte[], char str[MAXN]);
 
 int main(){
     FILE *fin = fopen("corse.txt", "r");
@@ -77,22 +85,23 @@ comando_e StampaMenu(){
 
 }
 
+//seleziona la funzione a seconda del comando
 void SelezionaFunzione(comando_e comando, int n, int *pfine, s_tratta Tratte[]){
     switch (comando) {
         case stampa:
             StampaCorse(n, Tratte);
             break;
         case DateOrd:
-            SortPrintDate(n, Tratte);
+            SortDate(n, Tratte);
             break;
         case CodOrd:
-            SortPrintCod(n, Tratte);
+            SortCod(n, Tratte);
             break;
         case PartOrd:
-            SortPrintPart(n, Tratte);
+            SortPart(n, Tratte);
             break;
         case DestOrd:
-            SortPrintDest(n, Tratte);
+            SortDest(n, Tratte);
             break;
         case search:
             RicercaTratta(n, Tratte);
@@ -103,5 +112,126 @@ void SelezionaFunzione(comando_e comando, int n, int *pfine, s_tratta Tratte[]){
         case err:
             printf("Comando non valido\n");
             break;
+    }
+}
+
+//stampa il contenuto del file log a video o su file a seconda di come specificato
+void StampaCorse(int n, s_tratta Tratte[]){
+    FILE *fp = NULL;
+    int scelta;
+    char nomefile[MAXN];
+    printf("Stampare a video (0) o su file (1)?\n");
+    scanf("%d", &scelta);
+    if (scelta){
+        printf("Inserire nome file:\n");
+        scanf("%s", nomefile);
+        fp = fopen(nomefile, "w");
+        for (int i = 0; i < n; i++){
+            fprintf(fp, "%s %s %s %s %s %s %d\n", Tratte[i].codt, Tratte[i].partenza, Tratte[i].destinazione, Tratte[i].data, Tratte[i].oraP, Tratte[i].oraA, Tratte[i].ritardo);
+        }
+        fclose(fp);
+    } else {
+        for (int i = 0; i < n; i++){
+            printf("%s %s %s %s %s %s %d\n", Tratte[i].codt, Tratte[i].partenza, Tratte[i].destinazione, Tratte[i].data, Tratte[i].oraP, Tratte[i].oraA, Tratte[i].ritardo);
+        }
+    }
+}
+
+//ordinamento tramite insertion sort rispetto a data e ora
+void SortDate(int n, s_tratta Tratte[]){
+    for (int i = 1; i < n; i++){
+        s_tratta x = Tratte[i];
+        int k = i-1;
+        while(k >= 0 && (strcmp(x.data, Tratte[k].data) < 0 || (strcmp(x.data, Tratte[k].data) == 0 && strcmp(x.oraP, Tratte[k].oraP) < 0))){
+            Tratte[k+1] = Tratte[k];
+            k--;
+        }
+        Tratte[k+1] = x;
+    }
+}
+
+//..... rispetto al codice
+void SortCod(int n, s_tratta Tratte[]){
+    for (int i = 1; i < n; i++){
+        s_tratta x = Tratte[i];
+        int k = i-1;
+        while(k >= 0 && strcmp(x.codt, Tratte[k].codt) < 0){
+            Tratte[k+1] = Tratte[k];
+            k--;
+        }
+        Tratte[k+1] = x;
+    }
+}
+
+//..... rispetto alla stazione di partenza
+void SortPart(int n, s_tratta Tratte[]){
+    for (int i = 1; i < n; i++){
+        s_tratta x = Tratte[i];
+        int k = i-1;
+        while(k >= 0 && strcmp(x.partenza, Tratte[k].partenza) < 0){
+            Tratte[k+1] = Tratte[k];
+            k--;
+        }
+        Tratte[k+1] = x;
+    }
+}
+
+//..... rispetto alla stazione di destinazione
+void SortDest(int n, s_tratta Tratte[]){
+    for (int i = 1; i < n; i++){
+        s_tratta x = Tratte[i];
+        int k = i-1;
+        while(k >= 0 && strcmp(x.destinazione, Tratte[k].destinazione) < 0){
+            Tratte[k+1] = Tratte[k];
+            k--;
+        }
+        Tratte[k+1] = x;
+    }
+}
+
+//funzione che chiama la ricerca lineare o dicotomica a seconda di come specificato
+void RicercaTratta(int n, s_tratta Tratte[]){
+    int scelta;
+    char partenza[MAXN];
+    printf("Inserire la stazione di partenza per la ricerca (in minuscolo):\n");
+    scanf("%s", partenza);
+    printf("Ricerca dicotomica (0) o lineare (1):\n");
+    scanf("%d", &scelta);
+    if (scelta){
+        linearSearch(n, Tratte, partenza);
+    } else {
+        SortPart(n, Tratte);
+        binarySearch(n, Tratte, partenza);
+    }
+}
+
+//ricerca lineare delle tratte con partenza uguale a str: si mette a minuscolo
+//la fermata presente in Tratte[i].partenza per usare str
+void linearSearch(int n, s_tratta Tratte[], char str[MAXN]){
+    char min[MAXN];
+    int j;
+    for (int i = 0; i < n; i++){
+
+        for (j = 0; j < strlen(Tratte[i].partenza); j++){
+            min[j] = tolower(Tratte[i].partenza[j]);
+        }
+        min[j] = '\0';
+
+        if (strstr(min, str) != NULL)
+            printf("%s %s %s %s %s %s %d\n", Tratte[i].codt, Tratte[i].partenza, Tratte[i].destinazione, Tratte[i].data, Tratte[i].oraP, Tratte[i].oraA, Tratte[i].ritardo);
+    }
+}
+
+//ricerca dicotomica ......
+void binarySearch(int n, s_tratta Tratte[], char str[MAXN]){
+    int l = 0, r = n-1, find = 0;
+    while(l <= r && !find){
+        int m = (l+r)/2;
+        if (strncasecmp(str, Tratte[m].partenza, strlen(str)) < 0) r = m-1;
+        else if (strncasecmp(str, Tratte[m].partenza, strlen(str)) > 0) l = m+1;
+        else if (strncasecmp(str, Tratte[m].partenza, strlen(str)) == 0) {
+            printf("%s %s %s %s %s %s %d\n", Tratte[m].codt, Tratte[m].partenza, Tratte[m].destinazione, Tratte[m].data, Tratte[m].oraP, Tratte[m].oraA, Tratte[m].ritardo);
+            find = 1;
+        }
     }
 }
