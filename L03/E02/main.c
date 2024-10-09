c7441d0faba70b8ff0fb18072cc2c62c2bce816b
AlessoBenvegna <147596860+AlessoBenvegna@users.noreply.github.com>
2024-10-09 15:30:33 +0200

    funzione leggifile completa

--- L03/E02/main.c
+++ L03/E02/main.c
@@ -2,58 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAXC 100
 #define MAXN 255
 
+//Struct che contiene un vettore di interi che si riferiscono
+//agli indici del vettore di tutte le canzoni, per capire quali
+//canzoni sono di quale amico, e la dimensione del suddetto vettore
 typedef struct{
-    char *s1;
-    char *s2;
-    char *s3;
-    char *s4;
-    char *s5;
-}songs;
+    int *IndiciCanzoni;
+    int numCanzoni;
+}s_CanzoniAmico;
 
-int LeggiFile(FILE *fp, songs **vp_songs);
+int LeggiFile(FILE *fp, s_CanzoniAmico **p_v_CA, char **Canzoni);
 
 int main(){
     FILE *fp = fopen("brani.txt", "r");
-    songs **vp_songs;
+    s_CanzoniAmico *v_CA; //vettore di struct
     int n_amici;
-
-    n_amici = LeggiFile(fp, vp_songs);
+    char *Canzoni[MAXC]; //vettore statico di puntatori a char, ovvero le canzoni, allocate dinamicamente dopo
+    n_amici = LeggiFile(fp, &v_CA, Canzoni);
 
 
 
     return 0;
 }
 
-int LeggiFile(FILE *fp, songs **vp_songs){
-    int na;
-    char song[MAXN];
-    fscanf(fp, "%d", &na);
-    vp_songs = malloc(na*sizeof(songs *));
-    for (int i = 0; i < na; i++){
+//La funzione riceve come parametri il vettore delle canzoni da riempire,
+//allocando dinamicamente le singole canzoni e un puntatore al vettore di struct per
+//ritornare by pointer il vettore delle struct completo. La funzione
+//legge il file e alloca dinamicamente il vettore delle struct e le canzoni
+int LeggiFile(FILE *fp, s_CanzoniAmico **p_v_CA, char **Canzoni){
+    s_CanzoniAmico *vca; //vettore di struct interno
+    char song[MAXN]; //canzone letta da file
+    int t = 0, numS; //t usato come indice nel vettore delle canzoni, numS è il numero di amici
+    fscanf(fp, "%d", &numS);
+    vca = malloc(numS*sizeof *vca); //allocazione vettore di struct
+    for (int i = 0; i < numS; i++) {
         int nc;
-        vp_songs[i] = malloc(sizeof *vp_songs[i]);
         fscanf(fp, "%d", &nc);
-        for (int j = 0; j < nc; j++){
+        vca[i].numCanzoni = nc;
+        vca[i].IndiciCanzoni = malloc(nc * sizeof(int)); //allocazione vettore di interi, indici di riferimento alle canzoni
+        for (int j = 0; j < nc; j++) {
             fscanf(fp, "%s", song);
-            vp_songs[i]->s1 = strdup(song);
-
+            Canzoni[t++] = strdup(song); //allocazione dimanica singole canzoni
+            vca[i].IndiciCanzoni[j] = t; //inserimento nel vettore di interi dell'indice in cui la canzone si trova la canzone nel vettore con tutte le altre
         }
     }
 
+    *p_v_CA = vca; //aggancio del puntatore al vettore di struct per ritornarlo al main
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return numS;
 }
