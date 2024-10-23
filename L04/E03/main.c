702635cda6313cde2ea0c316fdcc63bf037d49e1
AlessoBenvegna <147596860+AlessoBenvegna@users.noreply.github.com>
2024-10-23 17:28:28 +0200

    controllare errore al 3 ciclo

--- L04/E03/main.c
+++ L04/E03/main.c
@@ -4,7 +4,7 @@
 
 
 void CercaCollana(int *val, int n);
-int CalcolaCollana(int pos, int *val, int *sol, int maxl, int n, int l);
+int CalcolaCollana(int pos, int *val, int *sol, int *fin, int l, int start);
 
 int main(){
 
@@ -19,35 +19,77 @@ int main(){
     }
 
     CercaCollana(pietre, n);
+    free(pietre);
 
     return 0;
 }
 
 void CercaCollana(int *val, int n){
-    int maxl = 0, pos = 0, l = 0;
+    int maxl = 0, l = 0, start = 0;
     for (int q = 0; q < n; q++) maxl += val[q];
     int *sol = malloc(maxl*sizeof (int));
+    int *fin = malloc(maxl*sizeof (int));
 
-    l = CalcolaCollana(pos, val, sol, maxl, n, l);
-    printf("La collana a lunghezza massima è lunga %d", l);
+    for (int k = 0; k < maxl; k++)
+        fin[k] = -1;
+
+    for (int h = 0; h < n; h++){
+        l = CalcolaCollana(h, val, sol, fin, l, start);
+
+        printf("\nLa collana a lunghezza massima e' lunga %d\n", l);
+        l = 0; start = 0;
+    }
+
+    int i;
+    for (i = 0; fin[i] != -1; i++){
+        if (fin[i] == 0) printf("%c ", 'z');
+        else if (fin[i] == 1) printf("%c ", 'r');
+        else if (fin[i] == 2) printf("%c ", 't');
+        else if (fin[i] == 3) printf("%c ", 's');
+    }
+    printf("\nLa collana a lunghezza massima e' lunga %d", i-1);
+
+    free(sol);
+    free(fin);
 
 }
 
-int CalcolaCollana(int pos, int *val, int *sol, int maxl, int n, int l){
+int CalcolaCollana(int pos, int *val, int *sol, int *fin, int l, int start){
+    int l1, l2;
 
+    if (val[pos] > 0){
+        sol[start] = pos;
+        val[pos]--;
+        l++;
 
+        if (pos == 0){
+            l1 = CalcolaCollana(pos, val, sol, fin, l, start+1);
+            l2 = CalcolaCollana(pos+1, val, sol, fin, l, start+1);
+        } else if (pos == 1){
+            l1 = CalcolaCollana(pos+1, val, sol, fin,  l, start+1);
+            l2 = CalcolaCollana(pos+2, val, sol, fin, l, start+1);
+        } else if (pos == 2){
+            l1 = CalcolaCollana(pos-1, val, sol, fin, l, start+1);
+            l2 = CalcolaCollana(pos-2, val, sol, fin, l, start+1);
+        } else {
+            l1 = CalcolaCollana(pos, val, sol, fin, l, start+1);
+            l2 = CalcolaCollana(pos-1, val, sol, fin, l, start+1);
+        }
 
+        val[pos]++;
+        if (l1 == 0 && l2 == 0){
+            int i;
+            for (i = 0; fin[i] != -1; i++);
+            printf("%d\n",i);
+            if (l > i)
+                for (int j = 0; j < l; j++)
+                    fin[j] = sol[j];
+            return l;
+        }
+        else if (l1 > l2)
+            return l1;
+        return l2;
+    }
+    return 0;
 
-
-
-
-
-
-
-
-
-
-
-
-    return l;
 }
