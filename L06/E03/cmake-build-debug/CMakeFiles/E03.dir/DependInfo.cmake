4a49272446b6d8700ca5d4d56008d5c70999bbac
AlessoBenvegna <147596860+AlessoBenvegna@users.noreply.github.com>
2024-11-18 13:01:33 +0100

    modulo inventario completo; modulo personaggi fatta la lettura file

---
+++ L06/E03/cmake-build-debug/CMakeFiles/E03.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/alebe/Documents/UNI Polito/ASD/L02/E03/main.c" "CMakeFiles/E03.dir/C_/Users/alebe/Documents/UNI_Polito/ASD/L02/E03/main.c.obj" "gcc" "CMakeFiles/E03.dir/C_/Users/alebe/Documents/UNI_Polito/ASD/L02/E03/main.c.obj.d"
+  "C:/Users/alebe/Documents/UNI Polito/ASD/L06/E03/inventario.c" "CMakeFiles/E03.dir/inventario.c.obj" "gcc" "CMakeFiles/E03.dir/inventario.c.obj.d"
+  "C:/Users/alebe/Documents/UNI Polito/ASD/L06/E03/main.c" "CMakeFiles/E03.dir/main.c.obj" "gcc" "CMakeFiles/E03.dir/main.c.obj.d"
+  "C:/Users/alebe/Documents/UNI Polito/ASD/L06/E03/personaggi.c" "CMakeFiles/E03.dir/personaggi.c.obj" "gcc" "CMakeFiles/E03.dir/personaggi.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ L06/E03/cmake-build-debug/CMakeFiles/E03.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for E03.
+# This may be replaced when dependencies are built.
